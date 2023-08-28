597050611209227eb16ea52b93f58eb145b556dd
Eva1ent <let@ik.me>
2023-08-29 05:28:23 +0800

    chore: update deps

--- scripts/doc-generate.ts
+++ scripts/doc-generate.ts
@@ -1,15 +1,13 @@
 import glob from "fast-glob";
-import { $, fs, path } from "zx";
+import path from "pathe";
+import { $, fs } from "zx";
 
 import { BASEURL_CONFIGS, BASEURL_RULE } from "../tools/create-eslint-rule";
 
 async function main() {
-    const files = await glob("src/rules/*.md");
+    const files = glob.sync("src/rules/*.md").map((x) => path.basename(x));
 
-    for (const file of files) {
-        const basename = path.basename(file);
-        await fs.copyFile(file, `docs/rules/${basename}`);
-    }
+    await Promise.all(files.map(async (file) => fs.copyFile(`src/rules/${file}`, `docs/rules/${file}`)));
 
     await $`eslint-doc-generator --rule-list-columns name,description,fixable,hasSuggestions,deprecated --path-rule-list README.MD --url-rule-doc ${BASEURL_RULE}/{name} --url-configs ${BASEURL_CONFIGS}/configs && eslint-doc-generator --rule-list-columns name,description,fixable,hasSuggestions,deprecated --rule-doc-title-format name --url-rule-doc /docs/rules/{name}.md --url-configs /configs/`;
 }
