48f8cc0a9016df5757a72d0af51ecc4d75710593
Eva1ent <let@ik.me>
2023-09-10 03:01:15 +0800

    build: replace pnpm with bun

--- src/utils/component-detector.ts
+++ src/utils/component-detector.ts
@@ -1,11 +1,10 @@
-/* eslint-disable sonarjs/no-duplicate-string */
 /**
  * This module is adapted from eslint-plugin-solid's
  * https://github.com/solidjs-community/eslint-plugin-solid/blob/main/src/rules/components-return-once.ts,
  * under the MIT license.
  *
  */
-/* eslint-disable @typescript-eslint/no-unused-vars */
+
 import * as MutList from "@effect/data/MutableList";
 import type { TSESTree } from "@typescript-eslint/utils";
 import { AST_NODE_TYPES } from "@typescript-eslint/utils";
@@ -13,6 +12,7 @@ import type { RuleContext, RuleListener } from "@typescript-eslint/utils/ts-esli
 import invariant from "tiny-invariant";
 
 import { AST } from "./ast";
+import { isReturningJSX } from "./jsx";
 
 export type FunctionNode =
     | TSESTree.ArrowFunctionExpression
@@ -21,10 +21,13 @@ export type FunctionNode =
 
 export type PossibleFunctionalComponent = FunctionNode & {};
 
-export function make<Ctx extends RuleContext<string, []>>(_: Ctx) {
+export function make<Ctx extends RuleContext<string, []>>(ctx: Ctx) {
     const components = new WeakSet<FunctionNode>();
 
-    const functionStack = MutList.make<{ isComponent: boolean; node: FunctionNode }>();
+    const functionStack = MutList.make<{
+        isComponent: boolean;
+        node: FunctionNode;
+    }>();
 
     const currentFunction = () => MutList.tail(functionStack);
 
@@ -53,27 +56,40 @@ export function make<Ctx extends RuleContext<string, []>>(_: Ctx) {
         "ArrowFunctionExpression:exit": onFunctionExit,
         "FunctionDeclaration:exit": onFunctionExit,
         "FunctionExpression:exit": onFunctionExit,
-        JSXElement() {
-            if (!MutList.isEmpty(functionStack)) {
-                const currentFn = currentFunction();
+        // JSXElement() {
+        //     if (!MutList.isEmpty(functionStack)) {
+        //         const currentFn = currentFunction();
 
-                invariant(currentFn, "Unexpected empty function stack");
+        //         invariant(currentFn, "Unexpected empty function stack");
 
-                currentFn.isComponent = true;
-                components.add(currentFn.node);
+        //         currentFn.isComponent = true;
+        //         components.add(currentFn.node);
+        //     }
+        // },
+        // JSXFragment() {
+        //     if (!MutList.isEmpty(functionStack)) {
+        //         const currentFn = currentFunction();
+
+        //         invariant(currentFn, "Unexpected empty function stack");
+
+        //         currentFn.isComponent = true;
+        //         components.add(currentFn.node);
+        //     }
+        // },
+        ReturnStatement(node) {
+            const returnStatements = AST.getNestedReturnStatements(node);
+
+            const hasJsx = returnStatements.some((returnStatement) => isReturningJSX(returnStatement, ctx));
+
+            if (!hasJsx || MutList.isEmpty(functionStack)) {
+                return;
             }
-        },
-        JSXFragment() {
-            if (!MutList.isEmpty(functionStack)) {
-                const currentFn = currentFunction();
 
-                invariant(currentFn, "Unexpected empty function stack");
-
-                currentFn.isComponent = true;
-                components.add(currentFn.node);
-            }
+            const currentFn = currentFunction();
+            invariant(currentFn, "Unexpected empty function stack");
+            currentFn.isComponent = true;
+            components.add(currentFn.node);
         },
-        // ReturnStatement(node) {},
     };
 
     return {
