e82a365bc3249249da2fef8c8c050cc0b2ce2eb7
Eva1ent <let@ik.me>
2023-09-29 12:46:42 +0800

    chore: reformat code

--- src/rules/no-dangerously-set-innerhtml-with-children.spec.ts
+++ src/rules/no-dangerously-set-innerhtml-with-children.spec.ts
@@ -11,39 +11,13 @@ const ruleTester = new RuleTester({
             jsx: true,
         },
         ecmaVersion: 2021,
-        sourceType: "module",
         project: "./tsconfig.json",
+        sourceType: "module",
         tsconfigRootDir: rootDir,
     },
 });
 
 ruleTester.run(RULE_NAME, rule, {
-    valid: [
-        ...validFunction.all,
-        "<div>Children</div>",
-        "<div {...props} />",
-        '<div dangerouslySetInnerHTML={{ __html: "HTML" }} />',
-        '<div children="Children" />',
-        `const props = { dangerouslySetInnerHTML: { __html: "HTML" } }
-        ;<div {...props} />`,
-        `const moreProps = { className: "eslint" }
-        const props = { children: "Children", ...moreProps }
-        ;<div {...props} />`,
-        `const otherProps = { children: "Children" }
-        const { a, b, ...props } = otherProps
-        ;<div {...props} />`,
-        "<App>Children</App>",
-        '<App dangerouslySetInnerHTML={{ __html: "HTML" }} />',
-        '<App dangerouslySetInnerHTML={{ __html: "HTML" }}>\n</App>',
-        'React.createElement("div", { dangerouslySetInnerHTML: { __html: "HTML" } })',
-        'React.createElement("div", {}, "Children")',
-        'React.createElement("App", { dangerouslySetInnerHTML: { __html: "HTML" } })',
-        'React.createElement("App", {}, "Children")',
-        "<App {...undefined}>Children</App>",
-        'React.createElement("App", undefined, "Children")',
-        `const props = { ...props, scratch: {mode: 'edit'} }
-        const component = shallow(<TaskEditableTitle {...props} />)`,
-    ],
     invalid: [
         {
             code: `<div dangerouslySetInnerHTML={{ __html: "HTML" }}>Children</div>`,
@@ -109,5 +83,31 @@ ruleTester.run(RULE_NAME, rule, {
             React.createElement("div", props)`,
             errors: [{ messageId: "INVALID" }],
         },
+    ],
+    valid: [
+        ...validFunction.all,
+        "<div>Children</div>",
+        "<div {...props} />",
+        '<div dangerouslySetInnerHTML={{ __html: "HTML" }} />',
+        '<div children="Children" />',
+        `const props = { dangerouslySetInnerHTML: { __html: "HTML" } }
+        ;<div {...props} />`,
+        `const moreProps = { className: "eslint" }
+        const props = { children: "Children", ...moreProps }
+        ;<div {...props} />`,
+        `const otherProps = { children: "Children" }
+        const { a, b, ...props } = otherProps
+        ;<div {...props} />`,
+        "<App>Children</App>",
+        '<App dangerouslySetInnerHTML={{ __html: "HTML" }} />',
+        '<App dangerouslySetInnerHTML={{ __html: "HTML" }}>\n</App>',
+        'React.createElement("div", { dangerouslySetInnerHTML: { __html: "HTML" } })',
+        'React.createElement("div", {}, "Children")',
+        'React.createElement("App", { dangerouslySetInnerHTML: { __html: "HTML" } })',
+        'React.createElement("App", {}, "Children")',
+        "<App {...undefined}>Children</App>",
+        'React.createElement("App", undefined, "Children")',
+        `const props = { ...props, scratch: {mode: 'edit'} }
+        const component = shallow(<TaskEditableTitle {...props} />)`,
     ],
 });
