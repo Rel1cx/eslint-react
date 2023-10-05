6b7e0a7c2fb515c1f96a018b1c1446c7cf5de93d
Eva1ent <let@ik.me>
2023-10-05 22:48:22 +0800

    refactor: optimize typing of isUnstableAssignmentPattern

--- src/utils/is-unstable-assignment-pattern.ts
+++ src/utils/is-unstable-assignment-pattern.ts
@@ -1,26 +1,35 @@
 import type { TSESTree } from "@typescript-eslint/types";
 import { AST_NODE_TYPES as N } from "@typescript-eslint/types";
 
+import type { Narrow } from "../lib/primitives";
 import * as AST from "./ast";
+
+export const unstableAssignmentPatternTypes = [
+    N.JSXElement,
+    N.ArrayExpression,
+    N.ObjectExpression,
+    N.FunctionExpression,
+    N.ArrowFunctionExpression,
+    N.ClassExpression,
+    N.NewExpression,
+    N.CallExpression,
+] as const;
 
 /**
  * Check if the given node is an unstable assignment pattern (will change between assignments)
  * @param node
  * @param node.right
  */
-export function isUnstableAssignmentPattern({ right }: TSESTree.AssignmentPattern) {
+export function isUnstableAssignmentPattern(node: TSESTree.AssignmentPattern): node is
+    & TSESTree.AssignmentPattern
+    & Narrow<{
+        right: TSESTree.RegExpLiteral | typeof unstableAssignmentPatternTypes[number];
+    }>
+{
+    const { right } = node;
     if (right.type === N.Literal) {
         return "regex" in right;
     }
 
-    return AST.isOneOf([
-        N.JSXElement,
-        N.ArrayExpression,
-        N.ObjectExpression,
-        N.FunctionExpression,
-        N.ArrowFunctionExpression,
-        N.ClassExpression,
-        N.NewExpression,
-        N.CallExpression,
-    ])(right);
+    return AST.isOneOf(unstableAssignmentPatternTypes)(right);
 }
