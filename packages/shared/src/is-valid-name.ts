ba6fd8b8fc45a1ab8818c2509ef420374429d986
Eva1ent <let@ik.me>
2023-10-12 10:09:27 +0800

    refactor(shared): remove unnecessary code

--- packages/shared/src/is-valid-name.ts
+++ packages/shared/src/is-valid-name.ts
@@ -1,20 +1,10 @@
-import { isNil } from "rambda";
-
-export const reComponentName = /^[A-Z]/u;
-export const reHookName = /^use[A-Z\d].*$/u;
+export const RE_COMPONENT_NAME = /^[A-Z]/u;
+export const RE_HOOK_NAME = /^use[A-Z\d].*$/u;
 
 export function isValidReactComponentName(name: null | string | undefined): name is string {
-    if (isNil(name)) {
-        return false;
-    }
-
-    return reComponentName.test(name);
+    return !!name && RE_COMPONENT_NAME.test(name);
 }
 
 export function isValidReactHookName(name: null | string | undefined): name is string {
-    if (isNil(name)) {
-        return false;
-    }
-
-    return reHookName.test(name);
+    return !!name && RE_HOOK_NAME.test(name);
 }
