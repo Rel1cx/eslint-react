ccbc278ce4de452621c819faabcf08f675b3afe9
Eva1ent <let@ik.me>
2023-10-15 11:02:49 +0800

    chore: update deps

--- taze.config.ts
+++ taze.config.ts
@@ -4,12 +4,8 @@ export default defineConfig({
     force: false,
     install: false,
     packageMode: {
-        // wait for plugins to be updated
-        rollup: "3.29.4",
-        // skip 1.3.1 because of a known issue
-        "string-ts": "1.3.0",
         // skip 1.10.15 because of a known issue
-        turbo: "1.10.14",
+        // turbo: "1.10.14",
     },
     write: true,
 });
