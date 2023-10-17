0bc9dae6954f6769a80756f109a6cc2e1dafc97f
Eva1ent <let@ik.me>
2023-10-17 14:52:23 +0800

    build: setup knip

---
+++ knip.ts
@@ -0,0 +1,28 @@
+import type { KnipConfig } from "knip";
+
+export default {
+    ignore: [".eslintrc.cjs", "**/*.config.ts", "**/*.d.ts", "**/fixtures"],
+    ignoreDependencies: [
+        "@effect/language-service",
+        "eslint-config-with-tsconfig",
+        "eslint-plugin-filenames-simple",
+        "eslint-plugin-functional-core",
+        "eslint-plugin-jsdoc",
+        "swc3",
+    ],
+    ignoreWorkspaces: [],
+    workspaces: {
+        ".": {
+            entry: ["scripts/*.ts", "**/*.config.ts"],
+            project: ["scripts/**/*.ts"],
+        },
+        "packages/*": {
+            entry: "src/index.ts",
+            project: "src/**/*.ts",
+        },
+        "packages/eslint-plugin": {
+            entry: ["src/index.ts", "src/rules/**/*.ts", "src/configs/*.ts"],
+            project: ["src/**/*.ts"],
+        },
+    },
+} satisfies KnipConfig;
