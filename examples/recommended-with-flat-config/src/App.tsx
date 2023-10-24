41198cc142fad3188f0e6abd31b135de8e7f5742
Eva1ent <let@ik.me>
2023-10-24 22:18:37 +0800

    refactor(examples): use a simple starter as example

---
+++ examples/recommended-with-flat-config/src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <div>
+            <div>
+                <a href="https://react.dev" target="_blank">
+                    <img src={reactLogo} className="logo react" alt="React logo" />
+                </a>
+            </div>
+            <div className="card">
+                <button type="button" onClick={() => setCount((count) => count + 1)}>
+                    count is {count}
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default App;
