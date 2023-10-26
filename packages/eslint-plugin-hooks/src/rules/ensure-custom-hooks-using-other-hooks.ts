f5d2f3a0fc8c777e2283d0ef35b2b45640684266
Stephen Zhou <hi@hyoban.cc>
2023-10-26 09:45:03 +0800

    update

--- packages/eslint-plugin-hooks/src/rules/ensure-custom-hooks-using-other-hooks.ts
+++ packages/eslint-plugin-hooks/src/rules/ensure-custom-hooks-using-other-hooks.ts
@@ -1,1 +1,52 @@
-export {};
+import { getFunctionIdentifier } from "@eslint-react/ast";
+import { hooksCollector } from "@eslint-react/hooks";
+import { createRule } from "@eslint-react/shared";
+import { E } from "@eslint-react/tools";
+import type { ESLintUtils } from "@typescript-eslint/utils";
+
+export const RULE_NAME = "ensure-custom-hooks-using-other-hooks";
+
+type MessageID = "CUSTOM_HOOKS_NOT_USING_OTHER_HOOKS";
+
+export default createRule<[], MessageID>({
+    name: RULE_NAME,
+    meta: {
+        type: "problem",
+        docs: {
+            description: "enforce custom hooks using other hooks",
+            requiresTypeChecking: false,
+        },
+        schema: [],
+        messages: {
+            CUSTOM_HOOKS_NOT_USING_OTHER_HOOKS: "Custom hooks {{name}} should use other hooks",
+        },
+    },
+    defaultOptions: [],
+    create(context) {
+        const { ctx, listeners } = hooksCollector(context);
+
+        return {
+            ...listeners,
+            "Program:exit"() {
+                const maybeRedundantHooks = ctx.getAllRedundantHooks();
+                if (E.isLeft(maybeRedundantHooks)) {
+                    console.error(maybeRedundantHooks.left);
+
+                    return;
+                }
+
+                const redundantHooks = maybeRedundantHooks.right;
+                for (const hook of redundantHooks) {
+                    const name = getFunctionIdentifier(hook)?.name ?? "unknown";
+                    context.report({
+                        data: {
+                            name,
+                        },
+                        messageId: "CUSTOM_HOOKS_NOT_USING_OTHER_HOOKS",
+                        node: hook,
+                    });
+                }
+            },
+        };
+    },
+});
