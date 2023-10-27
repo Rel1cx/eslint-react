4b0a3b7148250fdb392c1619cf7e15f02ec48d4d
Stephen Zhou <hi@hyoban.cc>
2023-10-27 18:36:39 +0800

    feat: support eslint define config (#33)

---
+++ packages/eslint-plugin/src/eslint-define-config.d.ts
@@ -0,0 +1,12 @@
+import type eslintPlugin from "./index";
+
+type Rules = typeof eslintPlugin.rules;
+type RulesWithOptions = {
+    [ruleName in keyof Rules as `@eslint-react/${ruleName}`]: Parameters<Rules[ruleName]["create"]>[0]["options"];
+};
+
+declare module "eslint-define-config" {
+    // eslint-disable-next-line @typescript-eslint/no-empty-interface
+    export interface CustomRuleOptions extends RulesWithOptions {
+    }
+}
