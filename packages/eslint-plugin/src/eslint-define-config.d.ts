f2a8e90c0e5b4919b463d890b159614348ca7b32
Eva1ent <let@ik.me>
2023-11-03 03:52:16 +0800

    fix: remove internal packages imports from dts, close #69 (#71)

--- packages/eslint-plugin/src/eslint-define-config.d.ts
+++ packages/eslint-plugin/src/eslint-define-config.d.ts
@@ -1,13 +1,9 @@
-import type { Pretty } from "@eslint-react/tools";
-
 import type eslintPlugin from "./index";
 
 type Rules = typeof eslintPlugin.rules;
-type RulesWithOptions = Pretty<
-  {
-    [ruleName in keyof Rules as `@eslint-react/${ruleName}`]: Parameters<Rules[ruleName]["create"]>[0]["options"];
-  }
->;
+type RulesWithOptions = {
+  [ruleName in keyof Rules as `@eslint-react/${ruleName}`]: Parameters<Rules[ruleName]["create"]>[0]["options"];
+};
 
 declare module "eslint-define-config" {
   // eslint-disable-next-line @typescript-eslint/no-empty-interface
