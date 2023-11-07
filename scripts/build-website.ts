fdb8c0f0f678b8176b0132dde475321a4cc20352
Eva1ent <let@ik.me>
2023-11-07 17:53:47 +0800

    refactor: update rule sorting in website

--- scripts/build-website.ts
+++ scripts/build-website.ts
@@ -9,6 +9,7 @@ const docs = glob.sync("packages/eslint-plugin-*/src/rules/*.md");
 
 const metas = MutableRef.make<Record<string, string>>({});
 const files = MutableList.make<[string, string]>();
+const order = ["jsx", "react", "hooks", "naming-convention", "debug"] as const;
 
 for (const doc of docs) {
   const namespace = /^packages\/eslint-plugin-([^/]+)/u.exec(doc)?.[1] ?? "";
@@ -23,8 +24,12 @@ for (const doc of docs) {
 await Promise.all([...files].map(([src, dest]) => copyFile(src, dest)));
 
 const metaFile = path.join("website", "pages", "rules", "_meta.json");
-
-MutableRef.update(metas, (m) => Object.fromEntries([...Object.entries(m)].sort(([a], [b]) => b.localeCompare(a))));
+MutableRef.update(metas, (m) =>
+  Object.fromEntries(
+    [...Object.entries(m)]
+      .sort(([a], [b]) => a.localeCompare(b))
+      .sort(([a], [b]) => order.findIndex((x) => a.startsWith(x)) - order.findIndex((x) => b.startsWith(x))),
+  ));
 await Bun.write(metaFile, JSON.stringify(MutableRef.get(metas), null, 2));
 Bun.spawnSync(["pnpm", "--filter", "website", "run", "build"], {
   stdio: ["inherit", "inherit", "inherit"],
