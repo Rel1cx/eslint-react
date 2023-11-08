c378d6e462cfcb1f549b43159b880d62aeafd733
Eva1ent <let@ik.me>
2023-11-08 23:32:46 +0800

    docs: update examples

--- examples/with-stylistic-eslint-plugin/eslint.config.js
+++ examples/with-stylistic-eslint-plugin/eslint.config.js
@@ -2,7 +2,7 @@ import ts from "@typescript-eslint/eslint-plugin";
 import tsParser from "@typescript-eslint/parser";
 import react from "@eslint-react/eslint-plugin";
 import reactHooks from "eslint-plugin-react-hooks";
-import stylisticJsx from "@stylistic/eslint-plugin-jsx";
+import stylistic from "@stylistic/eslint-plugin";
 
 export default [
   {
@@ -14,14 +14,13 @@ export default [
     plugins: {
       "@typescript-eslint": ts,
       "react-hooks": reactHooks,
-      "@stylistic/jsx": stylisticJsx,
+      "@stylistic": stylistic,
     },
     rules: {
       ...ts.configs["eslint-recommended"].rules,
       ...ts.configs["recommended"].rules,
       ...reactHooks.configs.recommended.rules,
-      "@stylistic/jsx/jsx-indent-props": ["error", 2],
-      "@stylistic/jsx/jsx-max-props-per-line": ["error", { maximum: 1, when: "multiline" }],
+      "@stylistic/jsx-indent": ["error", 2],
       // ... see https://eslint.style/packages/jsx#rules
     },
   },
