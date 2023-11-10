0c3ad5bce21736f31c81f0c328c561285e42e5ab
Eva1ent <let@ik.me>
2023-11-10 22:49:34 +0800

    refactor: update getDocsUrl in createRule to use website urls (#115)

--- taze.config.ts
+++ taze.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   install: false,
   packageMode: {
     // Synchronize the version of this package with the patch
-    "eslint-doc-generator": "1.5.2",
+    "eslint-doc-generator": "1.5.4",
   },
   write: true,
 });
