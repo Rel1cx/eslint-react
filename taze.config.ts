619fbe9b771e6a976ff8a41e440fa7bdbe33950e
Stephen Zhou <hi@hyoban.cc>
2023-11-14 17:31:43 +0800

    build: remove patch of eslint-doc-generator

--- taze.config.ts
+++ taze.config.ts
@@ -3,9 +3,6 @@ import { defineConfig } from "taze";
 export default defineConfig({
   force: false,
   install: false,
-  packageMode: {
-    // Synchronize the version of this package with the patch
-    "eslint-doc-generator": "1.5.4",
-  },
+  packageMode: {},
   write: true,
 });
