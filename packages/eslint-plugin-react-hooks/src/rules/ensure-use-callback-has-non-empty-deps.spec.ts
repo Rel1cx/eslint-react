2a3111b3f4791e129e52f4b47587bfe7462c3111
Eva1ent <let@ik.me>
2023-11-19 04:58:52 +0800

    add ensure-use-xxx-has-non-empty-deps, closes #157, #158 (#163)

--- packages/eslint-plugin-react-hooks/src/rules/ensure-use-callback-has-non-empty-deps.spec.ts
+++ packages/eslint-plugin-react-hooks/src/rules/ensure-use-callback-has-non-empty-deps.spec.ts
@@ -23,8 +23,75 @@ const ruleTester = new RuleTester({
 ruleTester.run(RULE_NAME, rule, {
   valid: [
     ...allValid,
+    dedent`
+      import { useState } from "react";
+
+      const Comp = () => {
+        const [state, setState] = useState(false);
+
+        return <Button />;
+      };
+    `,
+    dedent`
+      const useData = (key) => {
+          return useSWR(key);
+      }
+    `,
+    dedent`
+      function useData(key) {
+          return useSWR(key);
+      }
+    `,
+    dedent`
+      function useData(key) {
+          const data = useSWR(key);
+          return data;
+      }
+    `,
+    dedent`
+      const useData = (key) => useSWR(key);
+    `,
+    dedent`
+      const onClick = () => {
+        console.log("clicked");
+      };
+
+      const Comp = () => {
+        return <Button onClick={onClick} />;
+      };
+    `,
+    dedent`
+      import { useCallback } from "react";
+
+      const Comp = ({ theme }) => {
+        const style = useCallback(() => ({
+          input: {
+            fontFamily: theme.fontFamilyMonospace
+          }
+        }), [theme.fontFamilyMonospace]);
+        return <Button sx={style} />
+      }
+    `,
   ],
   invalid: [
+    {
+      code: dedent`
+        import { useCallback } from "react";
+
+        const Comp = () => {
+            const onClick = useCallback(() => {
+              console.log("clicked");
+            }, []);
+
+            return <Button onClick={onClick} />;
+          };
+      `,
+      errors: [
+        {
+          messageId: "ENSURE_USE_CALLBACK_HAS_NON_EMPTY_DEPS",
+        },
+      ],
+    },
     {
       code: dedent`
         import { useCallback } from "react";
