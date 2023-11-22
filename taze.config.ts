ad75266484e81834de965d865c405a72a05436a6
Eva1ent <let@ik.me>
2023-11-22 17:13:13 +0800

    chore: update deps

--- taze.config.ts
+++ taze.config.ts
@@ -3,6 +3,13 @@ import { defineConfig } from "taze";
 export default defineConfig({
   force: false,
   install: false,
-  packageMode: {},
+  packageMode: {
+    "@typescript-eslint/eslint-plugin": "6.10.0",
+    "@typescript-eslint/parser": "6.10.0",
+    "@typescript-eslint/scope-manager": "6.10.0",
+    "@typescript-eslint/type-utils": "6.10.0",
+    "@typescript-eslint/types": "6.10.0",
+    "@typescript-eslint/utils": "6.10.0",
+  },
   write: true,
 });
