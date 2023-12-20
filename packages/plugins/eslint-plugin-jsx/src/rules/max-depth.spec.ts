1fb197ab90f5962a1995f933d23308994d006bfe
Eva1ent <let@ik.me>
2023-12-21 04:04:57 +0800

    test: add invalid cases for rule jsx/max-depth

--- packages/plugins/eslint-plugin-jsx/src/rules/max-depth.spec.ts
+++ packages/plugins/eslint-plugin-jsx/src/rules/max-depth.spec.ts
@@ -10,20 +10,46 @@ ruleTester.run(RULE_NAME, rule, {
   valid: [
     ...allValid,
     "const a = <div />;",
-    "const a = <div><div /></div>;",
-    "const a = <div><div><div /></div></div>;",
-    "const a = <div><div><div><div /></div></div></div>;",
-    "const a = <div><div><div><div><div /></div></div></div></div>;",
-    "const a = <div><div><div><div><div><div /></div></div></div></div></div>;",
-    "const a = <div><div><div><div><div><div><div /></div></div></div></div></div></div>;",
-    "const a = <div><div><div><div><div><div><div><div /></div></div></div></div></div></div></div>;",
-    "const a = <div><div><div><div><div><div><div><div><div /></div></div></div></div></div></div></div></div>;",
-    "const a = <div><div><div><div><div><div><div><div><div><div /></div></div></div></div></div></div></div></div></div>;",
+    "const a = <div><img /></div>;",
+    "const a = <div><div><img /></div></div>;",
+    "const a = <div><div><div><img /></div></div></div>;",
+    "const a = <div><div><div><div><img /></div></div></div></div>;",
+    "const a = <div><div><div><div><div><img /></div></div></div></div></div>;",
+    "const a = <div><div><div><div><div><div><img /></div></div></div></div></div></div>;",
+    "const a = <div><div><div><div><div><div><div><img /></div></div></div></div></div></div></div>;",
+    "const a = <div><div><div><div><div><div><div><div><img /></div></div></div></div></div></div></div></div>;",
+    "const a = <div><div><div><div><div><div><div><div><div><img /></div></div></div></div></div></div></div></div></div>;",
     {
-      code: "const a = <div><div><div>{<div />}</div></div></div>;",
+      code: "const a = <div><div><div>{<img />}</div></div></div>;",
       options: [{ max: 4 }],
     },
   ],
-  // TODO: Add invalid tests
-  invalid: [],
+  invalid: [
+    {
+      code: "const a = <div><div><div><img /></div></div></div>;",
+      options: [{ max: 3 }],
+      errors: [
+        {
+          messageId: "MAX_DEPTH",
+          data: {
+            maxDepth: 3,
+            depth: 4,
+          },
+        },
+      ],
+    },
+    {
+      code: "const a = <div><div><div>{<img />}</div></div></div>;",
+      options: [{ max: 3 }],
+      errors: [
+        {
+          messageId: "MAX_DEPTH",
+          data: {
+            maxDepth: 3,
+            depth: 4,
+          },
+        },
+      ],
+    },
+  ],
 });
