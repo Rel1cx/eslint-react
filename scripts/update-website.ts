d8fdcf712dffffa5ad560265a6118c16290be103
Eva1ent <let@ik.me>
2023-12-21 04:29:23 +0800

    build: update glob in update:website npm script

--- scripts/update-website.ts
+++ scripts/update-website.ts
@@ -4,14 +4,14 @@ import path from "path";
 
 import { copyFile } from "./lib/fs";
 
-const docs = new Bun.Glob("packages/eslint-plugin-*/src/rules/*.md").scanSync();
+const docs = new Bun.Glob("packages/plugins/eslint-plugin-*/src/rules/*.md").scanSync();
 
 const order = ["jsx", "react", "react-hooks", "naming-convention", "debug"] as const;
 const files = MutList.make<[string, string]>();
 const metas = MutRef.make<Record<string, Record<string, string> | string>>({});
 
 for (const doc of docs) {
-  const namespace = /^packages\/eslint-plugin-([^/]+)/u.exec(doc)?.[1] ?? "";
+  const namespace = /^packages\/plugins\/eslint-plugin-([^/]+)/u.exec(doc)?.[1] ?? "";
   const basename = path.parse(path.basename(doc)).name;
   const filename = `${namespace}-${basename}`;
   const title = `${namespace}/${basename}`;
