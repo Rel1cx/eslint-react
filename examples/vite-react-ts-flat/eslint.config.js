3e45a4f8c0316c39a6a9498783129274020be211
Eva1ent <let@ik.me>
2023-12-25 15:09:06 +0800

    docs: improve config examples

--- examples/vite-react-ts-flat/eslint.config.js
+++ examples/vite-react-ts-flat/eslint.config.js
@@ -1,7 +1,7 @@
 import ts from "@typescript-eslint/eslint-plugin";
 import tsParser from "@typescript-eslint/parser";
-import react from "@eslint-react/eslint-plugin";
 import reactHooks from "eslint-plugin-react-hooks";
+import eslintReact from "@eslint-react/eslint-plugin";
 
 export default [
   // TypeScript rules
@@ -37,7 +37,7 @@ export default [
   // React rules
   {
     files: ["src/**/*.{ts,tsx}"],
-    ...react.configs.recommended,
+    ...eslintReact.configs.recommended,
   },
   // Configurations rules
   {
