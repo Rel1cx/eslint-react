6abfe58076ce771c5aa2dfcd5a1943d2bc672cf6
Eva1ent <let@ik.me>
2023-12-28 15:16:23 +0800

    refactor(website): improve Image component

--- website/components/image.tsx
+++ website/components/image.tsx
@@ -1,8 +1,9 @@
 import type { ImageProps as NextImageProps } from "next/image";
 import NextImage from "next/image";
+import React from "react";
 
-export interface ImageProps extends NextImageProps {}
-
-export function Image({ alt, quality = 100, src, ...rest }: ImageProps) {
-  return <NextImage alt={alt} quality={quality} src={src} {...rest} />;
-}
+export const Image = React.memo(
+  React.forwardRef<HTMLImageElement, NextImageProps>(function Image({ alt, quality = 100, src, ...rest }, ref) {
+    return <NextImage ref={ref} alt={alt} quality={quality} src={src} {...rest} />;
+  }),
+);
