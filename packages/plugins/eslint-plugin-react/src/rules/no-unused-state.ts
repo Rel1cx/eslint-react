c1b4479b1d898fe3efccb727bf27f2ffed23e3c5
Eva1ent <let@ik.me>
2024-01-11 05:20:30 +0800

    feat: add react/no-unused-state, closes #294 (#296)

---
+++ packages/plugins/eslint-plugin-react/src/rules/no-unused-state.ts
@@ -0,0 +1,133 @@
+import { getClassIdentifier, isOneOf, NodeType, traverseUp, type TSESTreeClass } from "@eslint-react/ast";
+import { isClassComponent } from "@eslint-react/core";
+import { _, F, MutList, O } from "@eslint-react/tools";
+import type { TSESTree } from "@typescript-eslint/utils";
+import type { ESLintUtils } from "@typescript-eslint/utils";
+import type { ConstantCase } from "string-ts";
+
+import { createRule } from "../utils";
+
+export const RULE_NAME = "no-unused-state";
+
+export type MessageID = ConstantCase<typeof RULE_NAME>;
+
+function isThisExpression(node: TSESTree.Expression): boolean {
+  if (node.type === NodeType.TSAsExpression) {
+    return isThisExpression(node.expression);
+  }
+
+  return node.type === NodeType.ThisExpression;
+}
+
+function getName(node: TSESTree.Expression | TSESTree.PrivateIdentifier): O.Option<string> {
+  if (node.type === NodeType.TSAsExpression) {
+    return getName(node.expression);
+  }
+  if (node.type === NodeType.Identifier || node.type === NodeType.PrivateIdentifier) {
+    return O.some(node.name);
+  }
+  if (node.type === NodeType.Literal) {
+    return O.some(String(node.value));
+  }
+  if (node.type === NodeType.TemplateLiteral && node.expressions.length === 0) {
+    return O.fromNullable(node.quasis[0]?.value.raw);
+  }
+
+  return O.none();
+}
+
+export default createRule<[], MessageID>({
+  name: RULE_NAME,
+  meta: {
+    type: "problem",
+    docs: {
+      description: "Prevents unused state of class component.",
+      recommended: "recommended",
+      requiresTypeChecking: false,
+    },
+    schema: [],
+    messages: {
+      NO_UNUSED_STATE: 'Unused state of class component "{{className}}".',
+    },
+  },
+  defaultOptions: [],
+  create(context) {
+    const classStack = MutList.make<TSESTreeClass>();
+    const methodStack = MutList.make<TSESTree.MethodDefinition | TSESTree.PropertyDefinition>();
+    const stateDefs = new WeakMap<TSESTreeClass, [O.Option<TSESTree.Node>, boolean]>();
+    function classEnter(node: TSESTreeClass) {
+      MutList.append(classStack, node);
+    }
+    function classExit() {
+      const currentClass = MutList.pop(classStack);
+      if (!currentClass || !isClassComponent(currentClass, context)) return;
+      const className = O.map(getClassIdentifier(currentClass), id => id.name);
+      const [def, isUsed] = stateDefs.get(currentClass) ?? [O.none(), false];
+      if (O.isNone(def) || isUsed) return;
+      context.report({
+        node: def.value,
+        messageId: "NO_UNUSED_STATE",
+        data: {
+          className: O.getOrElse(className, () => "Component"),
+        },
+      });
+    }
+    function methodEnter(node: TSESTree.MethodDefinition | TSESTree.PropertyDefinition) {
+      MutList.append(methodStack, node);
+      const currentClass = MutList.tail(classStack);
+      if (!currentClass || !isClassComponent(currentClass, context)) return;
+      if (node.static) return;
+      if (O.exists(getName(node.key), name => name === "state")) {
+        stateDefs.set(currentClass, [O.some(node.key), false]);
+      }
+    }
+    function methodExit() {
+      MutList.pop(methodStack);
+    }
+
+    return {
+      ClassDeclaration: classEnter,
+      ClassExpression: classEnter,
+      "ClassDeclaration:exit": classExit,
+      "ClassExpression:exit": classExit,
+      MethodDefinition: methodEnter,
+      PropertyDefinition: methodEnter,
+      "MethodDefinition:exit": methodExit,
+      "PropertyDefinition:exit": methodExit,
+      MemberExpression(node) {
+        if (!isThisExpression(node.object)) return;
+        if (!O.exists(getName(node.property), name => name === "state")) return;
+        const currentClass = MutList.tail(classStack);
+        if (!currentClass || !isClassComponent(currentClass, context)) return;
+        const currentMethod = MutList.tail(methodStack);
+        if (!currentMethod || currentMethod.static) return;
+        if (!currentClass.body.body.includes(currentMethod)) return;
+        const wrappedByNonArrowFunction = F.pipe(
+          node,
+          traverseUp(
+            n =>
+              isOneOf([
+                NodeType.FunctionDeclaration,
+                NodeType.FunctionExpression,
+              ])(n) || n === currentMethod,
+          ),
+          O.exists(n => {
+            if (n.type === NodeType.FunctionDeclaration) return true;
+            if (n.type === NodeType.FunctionExpression) {
+              return F.pipe(
+                n,
+                traverseUp(isOneOf([NodeType.MethodDefinition, NodeType.PropertyDefinition])),
+                O.exists(m => m !== currentMethod),
+              );
+            }
+
+            return false;
+          }),
+        );
+        if (wrappedByNonArrowFunction) return;
+        const [def] = stateDefs.get(currentClass) ?? [O.none(), false];
+        stateDefs.set(currentClass, [def, true]);
+      },
+    };
+  },
+}) satisfies ESLintUtils.RuleModule<MessageID>;
--- packages/utilities/ast/src/node/traverse.ts
+++ packages/utilities/ast/src/node/traverse.ts
@@ -1,4 +1,4 @@
-import { O } from "@eslint-react/tools";
+import { F, O } from "@eslint-react/tools";
 import type { TSESTree } from "@typescript-eslint/types";
 
 import { NodeType } from "./node-type";
@@ -9,14 +9,17 @@ import { NodeType } from "./node-type";
  * @param predicate The predicate to check each node
  * @returns The first node that matches the predicate or `null` if no node matches
  */
-export function traverseUp(node: TSESTree.Node, predicate: (node: TSESTree.Node) => boolean): O.Option<TSESTree.Node> {
+export const traverseUp: {
+  (predicate: (node: TSESTree.Node) => boolean): (node: TSESTree.Node) => O.Option<TSESTree.Node>;
+  (node: TSESTree.Node, predicate: (node: TSESTree.Node) => boolean): O.Option<TSESTree.Node>;
+} = F.dual(2, (node: TSESTree.Node, predicate: (node: TSESTree.Node) => boolean): O.Option<TSESTree.Node> => {
   const { parent } = node;
   if (!parent || parent.type === NodeType.Program) return O.none();
 
   return predicate(parent)
     ? O.some(parent)
     : traverseUp(parent, predicate);
-}
+});
 
 /**
  * Traverses up the AST tree until the predicate returns `true` or the root node is reached
@@ -25,14 +28,17 @@ export function traverseUp(node: TSESTree.Node, predicate: (node: TSESTree.Node)
  * @param predicate The predicate to check each node. **must be a type guard**
  * @returns The first node that matches the predicate or `null` if no node matches
  */
-export function traverseUpGuard<T extends TSESTree.Node>(
+export const traverseUpGuard: {
+  <T extends TSESTree.Node>(predicate: (node: TSESTree.Node) => node is T): (node: TSESTree.Node) => O.Option<T>;
+  <T extends TSESTree.Node>(node: TSESTree.Node, predicate: (node: TSESTree.Node) => node is T): O.Option<T>;
+} = F.dual(2, <T extends TSESTree.Node>(
   node: TSESTree.Node,
   predicate: (node: TSESTree.Node) => node is T,
-): O.Option<T> {
+): O.Option<T> => {
   const { parent } = node;
   if (!parent || parent.type === NodeType.Program) return O.none();
 
   return predicate(parent)
     ? O.some(parent)
     : traverseUpGuard(parent, predicate);
-}
+});
