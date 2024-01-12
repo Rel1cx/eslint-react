478ea797a6e5fd74b028cb927950951a9e6dbffc
Eva1ent <let@ik.me>
2024-01-13 03:48:41 +0800

    build: update deps

--- taze.config.ts
+++ taze.config.ts
@@ -4,12 +4,12 @@ export default defineConfig({
   force: false,
   install: false,
   packageMode: {
-    "@typescript-eslint/eslint-plugin": "6.18.1",
-    "@typescript-eslint/parser": "6.18.1",
-    "@typescript-eslint/scope-manager": "6.18.1",
-    "@typescript-eslint/type-utils": "6.18.1",
-    "@typescript-eslint/types": "6.18.1",
-    "@typescript-eslint/utils": "6.18.1",
+    "@typescript-eslint/eslint-plugin": ">=6.18.1",
+    "@typescript-eslint/parser": ">=6.18.1",
+    "@typescript-eslint/scope-manager": ">=6.18.1",
+    "@typescript-eslint/type-utils": ">=6.18.1",
+    "@typescript-eslint/types": ">=6.18.1",
+    "@typescript-eslint/utils": ">=6.18.1",
   },
   write: true,
 });
