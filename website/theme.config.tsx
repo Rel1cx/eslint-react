55e6b8f4af39758be8191539de9e228903369d3e
Eva1ent <let@ik.me>
2024-01-30 13:43:33 +0800

    docs: optimize logo

--- website/theme.config.tsx
+++ website/theme.config.tsx
@@ -50,14 +50,7 @@ export default {
       <link
         rel="icon"
         type="image/png"
-        sizes="32x32"
-        href={`${PUBLIC_URL}/favicon-32x32.png`}
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href={`${PUBLIC_URL}/favicon-16x16.png`}
+        href={`${PUBLIC_URL}/favicon.png`}
       />
       <link rel="preconnect" href="https://cdn.jsdelivr.net" />
     </>
