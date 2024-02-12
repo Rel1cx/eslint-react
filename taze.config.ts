3d57b6b25ad0b59ef0fbe084b362f8b7bd02829a
Eva1ent <rel1cx@icloud.com>
2024-02-13 04:13:41 +0800

    release: 1.5.4-next.3

--- taze.config.ts
+++ taze.config.ts
@@ -3,13 +3,5 @@ import { defineConfig } from "taze";
 export default defineConfig({
   force: false,
   install: false,
-  packageMode: {
-    "@typescript-eslint/eslint-plugin": ">=7.0.0",
-    "@typescript-eslint/parser": ">=7.0.0",
-    "@typescript-eslint/scope-manager": ">=7.0.0",
-    "@typescript-eslint/type-utils": ">=7.0.0",
-    "@typescript-eslint/types": ">=7.0.0",
-    "@typescript-eslint/utils": ">=7.0.0",
-  },
   write: true,
 });
