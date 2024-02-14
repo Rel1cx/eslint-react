f06a7918382fc24f590e228c14ee217c22e40f6f
Eva1ent <rel1cx@icloud.com>
2024-02-14 21:27:38 +0800

    refactor(website): remove unused code

--- website/pages/_app.tsx
+++ website/pages/_app.tsx
@@ -6,16 +6,6 @@ import "#/styles/fonts/monaspace/fonts.css";
 
 import type { AppProps } from "next/app";
 
-// import TwoslashPatch from "#/components/twoslash-patch";
-// import { useCustomStyle } from "#/hooks/use-custom-style";
-
 export default function App({ Component, pageProps }: AppProps) {
-  // useCustomStyle();
-
-  return (
-    <>
-      {/* <TwoslashPatch /> */}
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
