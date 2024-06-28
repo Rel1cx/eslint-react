dcb84c8914ff64a6730ce263984e85e6c3251d35
Eva1ent <let@ik.me>
2024-06-28 12:57:13 +0800

    refactor: improve rule 'no-useless-fragment'

--- packages/plugins/eslint-plugin-react-x/src/rules/no-useless-fragment.ts
+++ packages/plugins/eslint-plugin-react-x/src/rules/no-useless-fragment.ts
@@ -3,7 +3,7 @@ import { isFragmentElement } from "@eslint-react/core";
 import { isBuiltInElement, isKeyedElement, isLiteral, isPaddingSpaces } from "@eslint-react/jsx";
 import type { RuleContext } from "@eslint-react/types";
 import type { ESLintUtils, TSESTree } from "@typescript-eslint/utils";
-import { isMatching } from "ts-pattern";
+import { isMatching, P } from "ts-pattern";
 
 import { createRule } from "../utils";
 
@@ -13,14 +13,12 @@ export type MessageID =
   | "NO_USELESS_FRAGMENT"
   | "NO_USELESS_FRAGMENT_IN_BUILT_IN";
 
-function checkNode(
+function check(
   node: TSESTree.JSXElement | TSESTree.JSXFragment,
   context: RuleContext,
 ) {
   if (isKeyedElement(node, context)) return;
-  if (isBuiltInElement(node.parent)) {
-    context.report({ messageId: "NO_USELESS_FRAGMENT_IN_BUILT_IN", node });
-  }
+  if (isBuiltInElement(node.parent)) context.report({ messageId: "NO_USELESS_FRAGMENT_IN_BUILT_IN", node });
   if (node.children.length === 0) return context.report({ messageId: "NO_USELESS_FRAGMENT", node });
   const isChildren = isOneOf([NodeType.JSXElement, NodeType.JSXFragment])(node.parent);
   const firstChildren = node.children[0];
@@ -28,14 +26,10 @@ function checkNode(
   if (node.children.length === 1 && isLiteral(firstChildren) && !isChildren) return;
   const nonPaddingChildren = node.children.filter((child) => !isPaddingSpaces(child));
   if (nonPaddingChildren.length > 1) return;
-  if (nonPaddingChildren.length === 0) {
-    return context.report({ messageId: "NO_USELESS_FRAGMENT", node });
-  }
+  if (nonPaddingChildren.length === 0) return context.report({ messageId: "NO_USELESS_FRAGMENT", node });
   const first = nonPaddingChildren[0];
-  if (!first || first.type === NodeType.JSXExpressionContainer) return;
-  if (!isMatching({ type: NodeType.JSXExpressionContainer, expression: NodeType.CallExpression })(first)) {
-    return context.report({ messageId: "NO_USELESS_FRAGMENT", node });
-  }
+  if (isMatching({ type: NodeType.JSXExpressionContainer, expression: P.not(NodeType.CallExpression) }, first)) return;
+  context.report({ messageId: "NO_USELESS_FRAGMENT", node });
 }
 
 export default createRule<[], MessageID>({
@@ -57,10 +51,10 @@ export default createRule<[], MessageID>({
     return {
       JSXElement(node) {
         if (!isFragmentElement(node, context)) return;
-        checkNode(node, context);
+        check(node, context);
       },
       JSXFragment(node) {
-        checkNode(node, context);
+        check(node, context);
       },
     };
   },
