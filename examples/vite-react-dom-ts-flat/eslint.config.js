e98ef3b0f6133608c3637be32c375f335800334c
Rel1cx <rel1cx@proton.me>
2024-07-07 04:16:51 +0800

    refactor: remove unused code

--- examples/vite-react-dom-ts-flat/eslint.config.js
+++ examples/vite-react-dom-ts-flat/eslint.config.js
@@ -4,6 +4,7 @@ import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 import react from "@eslint-react/eslint-plugin";
 import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
 
 export default [
   // JavaScript rules
@@ -40,6 +41,15 @@ export default [
       "react-hooks": reactHooks,
     },
     rules: reactHooks.configs.recommended.rules,
+  },
+  {
+    files: ["src/**/*.tsx"],
+    plugins: {
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      "react-refresh/only-export-components": "warn",
+    },
   },
   // Disable type checking for JavaScript files
   {
--- packages/plugins/eslint-plugin/src/utils/pad-keys.ts
+++ packages/plugins/eslint-plugin/src/utils/pad-keys.ts
@@ -9,13 +9,3 @@ export function padKeysLeft<const T, const U extends string = "">(
 ): PadKeysLeft<T, U> {
   return transformKeys(obj, (key) => `${left}${key}`) as never;
 }
-
-export type PadKeysRight<T, U extends string = ""> = T extends [] ? T
-  : { [K in keyof T as `${Extract<K, string>}${U}`]: T[K] };
-
-export function padKeysRight<const T, const U extends string = "">(
-  obj: T,
-  right: U,
-): PadKeysRight<T, U> {
-  return transformKeys(obj, (key) => `${key}${right}`) as never;
-}
