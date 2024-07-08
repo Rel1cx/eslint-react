6e7c605b5629418f4a9ddadd5d91895bb3962d9c
Rel1cx <rel1cx@proton.me>
2024-07-09 03:52:35 +0800

    refactor: revert enhance 'debug/function-component' to report props and hook-calls names

--- packages/plugins/eslint-plugin-react-debug/src/rules/function-component.ts
+++ packages/plugins/eslint-plugin-react-debug/src/rules/function-component.ts
@@ -1,13 +1,7 @@
-import type { TSESTreeFunction } from "@eslint-react/ast";
-import { NodeType } from "@eslint-react/ast";
 import { ERFunctionComponentFlag, useComponentCollector } from "@eslint-react/core";
-import type { RuleContext } from "@eslint-react/types";
-import { getConstrainedTypeAtLocation } from "@typescript-eslint/type-utils";
-import type { ParserServicesWithTypeInformation } from "@typescript-eslint/utils";
-import { ESLintUtils } from "@typescript-eslint/utils";
+import type { ESLintUtils } from "@typescript-eslint/utils";
 import { Function as F, Option as O } from "effect";
 import type { ConstantCase } from "string-ts";
-import { unionTypeParts } from "ts-api-utils";
 
 import { createRule } from "../utils";
 
@@ -23,28 +17,12 @@ export default createRule<[], MessageID>({
     },
     messages: {
       FUNCTION_COMPONENT:
-        "[function component] name: {{name}}, memo: {{memo}}, forwardRef: {{forwardRef}}, props: {{props}}, hookCalls: {{hookCalls}}",
+        "[function component] name: {{name}}, memo: {{memo}}, forwardRef: {{forwardRef}}, hookCalls: {{hookCalls}}",
     },
     schema: [],
   },
   name: RULE_NAME,
   create(context) {
-    const services = ESLintUtils.getParserServices(context);
-    function getProps(node: TSESTreeFunction, context: RuleContext, services: ParserServicesWithTypeInformation) {
-      const [props] = node.params;
-      if (!props) return [];
-      if (props.type === NodeType.ObjectPattern) {
-        return props.properties.map(p => context.sourceCode.getText(p));
-      }
-      const propsType = getConstrainedTypeAtLocation(services, props);
-      const variants = unionTypeParts(propsType);
-      if (variants.length === 1 && variants[0]) {
-        return variants[0]
-          .getProperties()
-          .map(p => p.getEscapedName().toString());
-      }
-      return variants.map(t => t.getProperties().map(p => p.getEscapedName().toString()));
-    }
     const { ctx, listeners } = useComponentCollector(context);
     return {
       ...listeners,
@@ -55,9 +33,8 @@ export default createRule<[], MessageID>({
             data: {
               name: O.getOrElse(name, F.constant("anonymous")),
               forwardRef: Boolean(flag & ERFunctionComponentFlag.ForwardRef),
-              hookCalls: hookCalls.map(h => context.sourceCode.getText(h.callee)),
+              hookCalls: hookCalls.length,
               memo: Boolean(flag & ERFunctionComponentFlag.Memo),
-              props: getProps(node, context, services),
             },
             messageId: "FUNCTION_COMPONENT",
             node,
