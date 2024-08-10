2a42e6cbba597753da523e8793c3a83eb62d987a
Eva1ent <rel1cx@proton.me>
2024-08-10 21:21:26 +0800

    release: 1.9.2-next.4

--- eslint.config.d.ts
+++ eslint.config.d.ts
@@ -1,5 +1,4 @@
 declare module "@eslint/js";
 declare module "eslint-plugin-eslint-comments";
 declare module "eslint-plugin-eslint-plugin";
-declare module "eslint-plugin-perfectionist";
 declare module "eslint-plugin-unicorn";
