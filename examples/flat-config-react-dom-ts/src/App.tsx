366904ffe6d0db45947651a8feb8b52fefadc0c3
Eva1ent <rel1cx@proton.me>
2024-08-13 06:15:47 +0800

    chore: remove unused code from examples

--- examples/flat-config-react-dom-ts/src/App.tsx
+++ examples/flat-config-react-dom-ts/src/App.tsx
@@ -15,7 +15,6 @@ function App() {
         </a>
       </div>
       <div className="card">
-        <input type="text" value={0} defaultValue={0} />
         <button type="button" onClick={() => setCount((count) => count + 1n)}>
           count is {count.toString()}
         </button>
