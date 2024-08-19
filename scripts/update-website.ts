4c40a2275fc5cbeca6078be6fb50b78bdea835ec
Eva1ent <rel1cx@proton.me>
2024-08-19 11:15:40 +0800

    ci: update scripts

--- scripts/update-website.ts
+++ scripts/update-website.ts
@@ -1,46 +1,50 @@
-// import { FileSystem } from "@effect/platform";
-// // import { NodeFileSystem, NodeRuntime } from "@effect/platform-node";
-// import { BunFileSystem, BunRuntime } from "@effect/platform-bun";
-// import CodeBlockWriter from "code-block-writer";
-// import { Effect } from "effect";
-// import path from "pathe";
-// import { globSync } from "tinyglobby";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import path from "node:path";
 
-// const docs = globSync(["packages/plugins/eslint-plugin-react-*/src/rules/*.md"]);
-// const order = ["dom", "hooks-extra", "naming-convention", "debug"] as const;
-// const [
-//   files,
-//   rules,
-// ] = Array.from(docs).reduce<readonly [[string, string][], [string, string][]]>(
-//   ([files, rules], doc) => {
-//     const namespace = /^packages\/plugins\/eslint-plugin-react-([^/]+)/u.exec(doc)?.[1] ?? "";
-//     const basename = path.parse(path.basename(doc)).name;
-//     const isCoreRule = namespace === "x";
-//     const name = isCoreRule ? basename : `${namespace}-${basename}`;
-//     const title = isCoreRule ? basename : `${namespace}/${basename}`;
-//     const dest = path.join("website", "pages", "rules", `${name}.mdx`);
-//     return [[...files, [doc, dest]], [...rules, [name, title]]] as const;
-//   },
-//   [[], []],
-// );
-// const metaFile = path.join("website", "pages", "docs", "rules", "_meta.ts");
-// const metaContent = {
-//   overview: {
-//     title: "Overview",
-//     theme: {
-//       layout: "full",
-//     },
-//   },
-//   // eslint-disable-next-line perfectionist/sort-objects
-//   "---": {
-//     type: "separator",
-//   },
-//   ...fromEntries(
-//     rules
-//       .sort(([a], [b]) => a.localeCompare(b, "en", { numeric: true }))
-//       .sort(([a], [b]) => order.findLastIndex((x) => a.startsWith(x)) - order.findLastIndex((x) => b.startsWith(x))),
-//   ),
-// };
+import { FileSystem } from "@effect/platform";
+// import { NodeFileSystem, NodeRuntime } from "@effect/platform-node";
+import { BunFileSystem, BunRuntime } from "@effect/platform-bun";
+// import CodeBlockWriter from "code-block-writer";
+import { Effect } from "effect";
+import { globSync } from "tinyglobby";
+
+// TODO: Update this script to support the new website content structure
+const docs = globSync(["packages/plugins/eslint-plugin-react-*/src/rules/*.md"]);
+const order = ["dom", "hooks-extra", "naming-convention", "debug"] as const;
+const [
+  files,
+  rules,
+] = Array.from(docs).reduce<readonly [[string, string][], [string, string][]]>(
+  ([files, rules], doc) => {
+    const namespace = /^packages\/plugins\/eslint-plugin-react-([^/]+)/u.exec(doc)?.[1] ?? "";
+    const basename = path.parse(path.basename(doc)).name;
+    const isCoreRule = namespace === "x";
+    const name = isCoreRule ? basename : `${namespace}-${basename}`;
+    const title = isCoreRule ? basename : `${namespace}/${basename}`;
+    const dest = path.join("website", "pages", "rules", `${name}.mdx`);
+    return [[...files, [doc, dest]], [...rules, [name, title]]] as const;
+  },
+  [[], []],
+);
+const metaFile = path.join("website", "pages", "docs", "rules", "_meta.ts");
+const metaContent = {
+  overview: {
+    title: "Overview",
+    theme: {
+      layout: "full",
+    },
+  },
+  // eslint-disable-next-line perfectionist/sort-objects
+  "---": {
+    type: "separator",
+  },
+  ...Object.fromEntries(
+    rules
+      .sort(([a], [b]) => a.localeCompare(b, "en", { numeric: true }))
+      .sort(([a], [b]) => order.findLastIndex((x) => a.startsWith(x)) - order.findLastIndex((x) => b.startsWith(x))),
+  ),
+};
+
 // const writer = new CodeBlockWriter({
 //   indentNumberOfSpaces: 2,
 //   newLine: "\n",
@@ -53,13 +57,13 @@
 //   }
 // });
 
-// const program = Effect.gen(function*(_) {
-//   const fs = yield* _(FileSystem.FileSystem);
-//   yield* _(Effect.orDie(fs.writeFileString(metaFile, writer.toString())));
-//   yield* _(Effect.all(files.map(([src, dest]) => fs.copyFile(src, dest))));
-// });
+const program = Effect.gen(function*(_) {
+  const fs = yield* _(FileSystem.FileSystem);
+  // yield* _(Effect.orDie(fs.writeFileString(metaFile, writer.toString())));
+  yield* _(Effect.all(files.map(([src, dest]) => fs.copyFile(src, dest))));
+});
 
-// const runnable = program.pipe(Effect.provide(BunFileSystem.layer));
+const runnable = program.pipe(Effect.provide(BunFileSystem.layer));
 
-// BunRuntime.runMain(runnable);
-// // NodeRuntime.runMain(runnable);
+BunRuntime.runMain(runnable);
+// NodeRuntime.runMain(runnable);
