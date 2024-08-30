e32c8c7784619e60dac3b90198dac7a319c59ee0
Eva1ent <rel1cx@proton.me>
2024-08-30 13:25:51 +0800

    refactor(shared): improve settings schema

--- packages/shared/src/settings.ts
+++ packages/shared/src/settings.ts
@@ -62,14 +62,14 @@ export const normalizeSettings = memoize((settings: ESLintReactSettings) => {
     ...settings,
     additionalComponents: additionalComponents.map((component) => ({
       ...component,
-      attributes: component.attributes.map((attr) => ({
+      attributes: component.attributes?.map((attr) => ({
         ...attr,
         as: attr.as ?? attr.name,
-      })),
+      })) ?? [],
       re: pm.makeRe(component.name, { fastpaths: true }),
     })),
     components: additionalComponents.reduce((acc, component) => {
-      const { name, as, attributes, selector } = component;
+      const { name, as, attributes = [], selector } = component;
       if (!name || !as || selector || attributes.length > 0) return acc;
       if (!/^[\w-]+$/u.test(name)) return acc;
       return acc.set(name, as);
