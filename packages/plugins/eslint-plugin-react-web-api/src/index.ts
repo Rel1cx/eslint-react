f2e3b28c619c6d9a62c527eca11be3a894bccddd
Eva1ent <rel1cx@proton.me>
2024-09-01 11:25:43 +0800

    docs: rebuild docs

--- packages/plugins/eslint-plugin-react-web-api/src/index.ts
+++ packages/plugins/eslint-plugin-react-web-api/src/index.ts
@@ -3,6 +3,7 @@ import type { ESLintReactSettings } from "@eslint-react/shared";
 import { name, version } from "../package.json";
 import noLeakedEventListener from "./rules/no-leaked-event-listener";
 import noLeakedInterval from "./rules/no-leaked-interval";
+import noLeakedResizeObserver from "./rules/no-leaked-resize-observer";
 import noLeakedTimeout from "./rules/no-leaked-timeout";
 
 export const meta = {
@@ -13,6 +14,7 @@ export const meta = {
 export const rules = {
   "no-leaked-event-listener": noLeakedEventListener,
   "no-leaked-interval": noLeakedInterval,
+  "no-leaked-resize-observer": noLeakedResizeObserver,
   "no-leaked-timeout": noLeakedTimeout,
 } as const;
 
