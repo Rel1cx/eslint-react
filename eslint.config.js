4ea6a6073599d0ef74cf3b2742cebf9df4dc8439
Eva1ent <rel1cx@proton.me>
2024-09-21 12:57:50 +0800

    chore: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import * as importx from "importx";
 // eslint-disable-next-line no-restricted-syntax
 const mod = await importx.import("./eslint.config.ts", {
   cache: true,
-  loader: "tsx",
+  loader: "native",
   parentURL: import.meta.url,
 });
 
