3853f4540f4dc057917de21235715ce3bf125808
Jordan Thomson <jordant@canva.com>
2024-10-03 21:44:45 +1000

    lint + refactor

--- packages/plugins/eslint-plugin-react-x/src/rules/prefer-react-namespace-import.ts
+++ packages/plugins/eslint-plugin-react-x/src/rules/prefer-react-namespace-import.ts
@@ -1,7 +1,7 @@
 import type { TSESTree } from "@typescript-eslint/types";
 import type { CamelCase } from "string-ts";
+
 import { createRule } from "../utils";
-import { ESLintUtils } from "@typescript-eslint/utils";
 
 export const RULE_NAME = "prefer-react-namespace-import";
 
@@ -27,25 +27,23 @@ export default createRule<[], MessageID>({
         context.report({
           messageId: "preferReactNamespaceImport",
           node: hasOtherSpecifiers ? node : node.parent,
-          *fix(fixer) {
+          fix(fixer) {
             const isTypeImport = node.parent.importKind === "type";
+            const importStringPrefix = `import${isTypeImport ? " type" : ""}`;
             if (!hasOtherSpecifiers) {
-              yield fixer.replaceText(
+              return fixer.replaceText(
                 node.parent,
-                `import ${isTypeImport ? "type " : ""}* as ${node.local.name} from 'react';`,
+                `${importStringPrefix} * as ${node.local.name} from 'react';`,
               );
-              return;
             }
 
             // remove the default specifier and prepend the namespace import specifier
             const specifiers = node.parent.specifiers.slice(1).map((n) => {
               return context.sourceCode.getText(n);
             }).join(",");
-            yield fixer.replaceText(
+            return fixer.replaceText(
               node.parent,
-              `import ${isTypeImport ? "type " : ""}* as ${node.local.name} from 'react';\nimport ${
-                isTypeImport ? "type " : ""
-              }{${specifiers}} from 'react';`,
+              `${importStringPrefix} * as ${node.local.name} from 'react';\n${importStringPrefix} {${specifiers}} from 'react';`,
             );
           },
         });
