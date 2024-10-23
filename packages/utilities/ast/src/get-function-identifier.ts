a8c62c9c4b7475c1b5265f9c62261b4010ca211f
Eva1ent <rel1cx@proton.me>
2024-10-23 10:51:23 +0800

    refactor: minor improvements

--- packages/utilities/ast/src/get-function-identifier.ts
+++ packages/utilities/ast/src/get-function-identifier.ts
@@ -15,63 +15,47 @@ import { isOneOf } from "./is";
 import type { TSESTreeFunction } from "./types";
 
 export function getFunctionIdentifier(node: TSESTree.Expression | TSESTreeFunction): O.Option<TSESTree.Identifier> {
-  // function MaybeComponent() {}
-  // const whatever = function MaybeComponent() {};
-  if ("id" in node && node.id) return O.some(node.id);
-  // const MaybeComponent = () => {};
-  if (
-    node.parent.type === AST_NODE_TYPES.VariableDeclarator
-    && node.parent.init === node
-    && node.parent.id.type === AST_NODE_TYPES.Identifier
-  ) {
-    return O.some(node.parent.id);
-  }
-  // MaybeComponent = () => {};
-  if (
-    node.parent.type === AST_NODE_TYPES.AssignmentExpression
-    && node.parent.right === node
-    && node.parent.operator === "="
-    && node.parent.left.type === AST_NODE_TYPES.Identifier
-  ) {
-    return O.some(node.parent.left);
-  }
-  // {MaybeComponent: () => {}}
-  // {MaybeComponent() {}}
-  if (
-    node.parent.type === AST_NODE_TYPES.Property
-    && node.parent.value === node
-    && !node.parent.computed
-    && node.parent.key.type === AST_NODE_TYPES.Identifier
-  ) {
-    return O.some(node.parent.key);
-  }
-
-  // class {MaybeComponent = () => {}}
-  // class {MaybeComponent() {}}
-  if (
-    isOneOf([AST_NODE_TYPES.MethodDefinition, AST_NODE_TYPES.PropertyDefinition])(node.parent)
-    && node.parent.value === node
-    && node.parent.key.type === AST_NODE_TYPES.Identifier
-  ) {
-    return O.some(node.parent.key);
-  }
-  // Follow spec convention for `IsAnonymousFunctionDefinition()` usage.
-  //
-  // const {MaybeComponent = () => {}} = {};
-  // ({MaybeComponent = () => {}} = {});
-  if (
-    node.parent.type === AST_NODE_TYPES.AssignmentPattern
-    && node.parent.right === node
-    && node.parent.left.type === AST_NODE_TYPES.Identifier
-  ) {
-    return O.some(node.parent.left);
-  }
-
-  if (
-    isOneOf([AST_NODE_TYPES.TSAsExpression, AST_NODE_TYPES.TSSatisfiesExpression])(node.parent)
-    && node.parent.expression === node
-  ) {
-    return getFunctionIdentifier(node.parent);
+  switch (true) {
+    // function MaybeComponent() {}
+    case "id" in node
+      && !!node.id:
+      return O.some(node.id);
+    // const whatever = function MaybeComponent() {};
+    case node.parent.type === AST_NODE_TYPES.VariableDeclarator
+      && node.parent.init === node
+      && node.parent.id.type === AST_NODE_TYPES.Identifier:
+      return O.some(node.parent.id);
+    // MaybeComponent = () => {};
+    case node.parent.type === AST_NODE_TYPES.AssignmentExpression
+      && node.parent.right === node
+      && node.parent.operator === "="
+      && node.parent.left.type === AST_NODE_TYPES.Identifier:
+      return O.some(node.parent.left);
+    // {MaybeComponent: () => {}}
+    // {MaybeComponent() {}}
+    case node.parent.type === AST_NODE_TYPES.Property
+      && node.parent.value === node
+      && !node.parent.computed
+      && node.parent.key.type === AST_NODE_TYPES.Identifier:
+      return O.some(node.parent.key);
+    // class {MaybeComponent = () => {}}
+    // class {MaybeComponent() {}}
+    case isOneOf([AST_NODE_TYPES.MethodDefinition, AST_NODE_TYPES.PropertyDefinition])(node.parent)
+      && node.parent.value === node
+      && node.parent.key.type === AST_NODE_TYPES.Identifier:
+      return O.some(node.parent.key);
+      // Follow spec convention for `IsAnonymousFunctionDefinition()` usage.
+      //
+      // const {MaybeComponent = () => {}} = {};
+      // ({MaybeComponent = () => {}} = {});
+    case node.parent.type === AST_NODE_TYPES.AssignmentPattern
+      && node.parent.right === node
+      && node.parent.left.type === AST_NODE_TYPES.Identifier:
+      return O.some(node.parent.left);
+    // const MaybeComponent = (() => {}) as FunctionComponent;
+    // const MaybeComponent = (() => {}) satisfies FunctionComponent;
+    case isOneOf([AST_NODE_TYPES.TSAsExpression, AST_NODE_TYPES.TSSatisfiesExpression])(node.parent):
+      return getFunctionIdentifier(node.parent);
   }
   return O.none();
 }
