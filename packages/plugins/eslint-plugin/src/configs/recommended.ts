2123b29317d31cfb7786f42a1019d08c0d9eefce
Eva1ent <rel1cx@proton.me>
2024-11-23 15:41:31 +0800

    refactor: minor improvements

--- packages/plugins/eslint-plugin/src/configs/recommended.ts
+++ packages/plugins/eslint-plugin/src/configs/recommended.ts
@@ -2,32 +2,29 @@ import type { RulePreset } from "@eslint-react/types";
 import reactDebug from "eslint-plugin-react-debug";
 import reactHooksExtra from "eslint-plugin-react-hooks-extra";
 import reactNamingConvention from "eslint-plugin-react-naming-convention";
-import reactWebApi from "eslint-plugin-react-web-api";
 
 import * as core from "./core";
 import * as dom from "./dom";
+import * as webApi from "./web-api";
 
 export const name = "@eslint-react/recommended";
 
 export const rules = {
   ...core.rules,
   ...dom.rules,
+  ...webApi.rules,
   "@eslint-react/hooks-extra/no-direct-set-state-in-use-effect": "warn",
   "@eslint-react/hooks-extra/no-redundant-custom-hook": "warn",
   "@eslint-react/hooks-extra/prefer-use-state-lazy-initialization": "warn",
-  "@eslint-react/web-api/no-leaked-event-listener": "warn",
-  "@eslint-react/web-api/no-leaked-interval": "warn",
-  "@eslint-react/web-api/no-leaked-resize-observer": "warn",
-  "@eslint-react/web-api/no-leaked-timeout": "warn",
 } as const satisfies RulePreset;
 
 export const plugins = {
   ...core.plugins,
   ...dom.plugins,
+  ...webApi.plugins,
   "@eslint-react/debug": reactDebug,
   "@eslint-react/hooks-extra": reactHooksExtra,
   "@eslint-react/naming-convention": reactNamingConvention,
-  "@eslint-react/web-api": reactWebApi,
 };
 
 export const settings = {
