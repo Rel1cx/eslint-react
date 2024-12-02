ccd15f2eea0e1add91d7c1f8274ddc6517f12b0c
Eva1ent <rel1cx@proton.me>
2024-12-03 04:22:19 +0800

    refactor: minor improvements

--- packages/utilities/var/src/is-node-value-equal.ts
+++ packages/utilities/var/src/is-node-value-equal.ts
@@ -28,70 +28,80 @@ export function isNodeValueEqual(
   b: TSESTree.Node,
   initialScopes: [Scope, Scope],
 ): boolean {
-  if (a.type === AST_NODE_TYPES.Literal && b.type === AST_NODE_TYPES.Literal) return a.value === b.value;
-  if (a.type === AST_NODE_TYPES.TemplateElement && b.type === AST_NODE_TYPES.TemplateElement) {
-    return a.value.cooked === b.value.cooked;
-  }
   const [aScope, bScope] = initialScopes;
-  const aStatic = getStaticValue(a, aScope);
-  const bStatic = getStaticValue(b, bScope);
-  if (aStatic && bStatic) return aStatic.value === bStatic.value;
-  if (a.type === AST_NODE_TYPES.Identifier && b.type === AST_NODE_TYPES.Identifier) {
-    const aVar = findVariable(a, aScope);
-    const bVar = findVariable(b, bScope);
-    const aVarNode = O.flatMap(aVar, getVariableNode(0));
-    const bVarNode = O.flatMap(bVar, getVariableNode(0));
-    const aVarNodeParent = O.flatMapNullable(aVarNode, (n) => n.parent);
-    const bVarNodeParent = O.flatMapNullable(bVarNode, (n) => n.parent);
-    const aDef = O.flatMap(aVar, getVariableDef(0));
-    const bDef = O.flatMap(bVar, getVariableDef(0));
-    const aDefParentParent = O.flatMapNullable(aDef, (d) => d.parent?.parent);
-    const bDefParentParent = O.flatMapNullable(bDef, (d) => d.parent?.parent);
-    switch (true) {
-      case O.exists(aVarNodeParent, AST.is(AST_NODE_TYPES.CallExpression))
-        && O.exists(bVarNodeParent, AST.is(AST_NODE_TYPES.CallExpression))
-        && O.exists(aVarNode, AST.isFunction)
-        && O.exists(bVarNode, AST.isFunction): {
-        const hasSameCallee = F.pipe(
-          O.Do,
-          O.bind("aCallee", () => O.map(aVarNodeParent, (n) => n.callee)),
-          O.bind("bCallee", () => O.map(bVarNodeParent, (n) => n.callee)),
-          O.exists(({ aCallee, bCallee }) => AST.isNodeEqual(aCallee, bCallee)),
-        );
-        if (!hasSameCallee) return false;
-        return F.pipe(
-          O.Do,
-          O.bind("aParams", () => O.map(aVarNode, (n) => n.params)),
-          O.bind("bParams", () => O.map(bVarNode, (n) => n.params)),
-          O.let("aPos", ({ aParams }) => aParams.findIndex((p) => AST.isNodeEqual(p, a))),
-          O.let("bPos", ({ bParams }) => bParams.findIndex((p) => AST.isNodeEqual(p, b))),
-          O.filter(({ aPos, bPos }) => aPos !== -1 && bPos !== -1),
-          O.exists(({ aPos, bPos }) => aPos === bPos),
-        );
-      }
-      case O.exists(aDefParentParent, AST.is(AST_NODE_TYPES.ForOfStatement))
-        && O.exists(bDefParentParent, AST.is(AST_NODE_TYPES.ForOfStatement)): {
-        return F.pipe(
-          O.Do,
-          O.bind("aRight", () => O.map(aDefParentParent, (d) => d.right)),
-          O.bind("bRight", () => O.map(bDefParentParent, (d) => d.right)),
-          O.exists(({ aRight, bRight }) => AST.isNodeEqual(aRight, bRight)),
-        );
-      }
-      default: {
-        return O.isSome(aVar) && O.isSome(bVar) && aVar.value === bVar.value;
+  switch (true) {
+    case a.type === AST_NODE_TYPES.Literal
+      && b.type === AST_NODE_TYPES.Literal: {
+      return a.value === b.value;
+    }
+    case a.type === AST_NODE_TYPES.TemplateElement
+      && b.type === AST_NODE_TYPES.TemplateElement: {
+      return a.value.cooked === b.value.cooked;
+    }
+    case a.type === AST_NODE_TYPES.Identifier
+      && b.type === AST_NODE_TYPES.Identifier: {
+      const aVar = findVariable(a, aScope);
+      const bVar = findVariable(b, bScope);
+      const aVarNode = O.flatMap(aVar, getVariableNode(0));
+      const bVarNode = O.flatMap(bVar, getVariableNode(0));
+      const aVarNodeParent = O.flatMapNullable(aVarNode, (n) => n.parent);
+      const bVarNodeParent = O.flatMapNullable(bVarNode, (n) => n.parent);
+      const aDef = O.flatMap(aVar, getVariableDef(0));
+      const bDef = O.flatMap(bVar, getVariableDef(0));
+      const aDefParentParent = O.flatMapNullable(aDef, (d) => d.parent?.parent);
+      const bDefParentParent = O.flatMapNullable(bDef, (d) => d.parent?.parent);
+      switch (true) {
+        case O.exists(aVarNodeParent, AST.is(AST_NODE_TYPES.CallExpression))
+          && O.exists(bVarNodeParent, AST.is(AST_NODE_TYPES.CallExpression))
+          && O.exists(aVarNode, AST.isFunction)
+          && O.exists(bVarNode, AST.isFunction): {
+          const hasSameCallee = F.pipe(
+            O.Do,
+            O.bind("aCallee", () => O.map(aVarNodeParent, (n) => n.callee)),
+            O.bind("bCallee", () => O.map(bVarNodeParent, (n) => n.callee)),
+            O.exists(({ aCallee, bCallee }) => AST.isNodeEqual(aCallee, bCallee)),
+          );
+          if (!hasSameCallee) return false;
+          return F.pipe(
+            O.Do,
+            O.bind("aParams", () => O.map(aVarNode, (n) => n.params)),
+            O.bind("bParams", () => O.map(bVarNode, (n) => n.params)),
+            O.let("aPos", ({ aParams }) => aParams.findIndex(AST.isNodeEqual(a))),
+            O.let("bPos", ({ bParams }) => bParams.findIndex(AST.isNodeEqual(b))),
+            O.filter(({ aPos, bPos }) => aPos !== -1 && bPos !== -1),
+            O.exists(({ aPos, bPos }) => aPos === bPos),
+          );
+        }
+        case O.exists(aDefParentParent, AST.is(AST_NODE_TYPES.ForOfStatement))
+          && O.exists(bDefParentParent, AST.is(AST_NODE_TYPES.ForOfStatement)): {
+          return F.pipe(
+            O.Do,
+            O.bind("aRight", () => O.map(aDefParentParent, (d) => d.right)),
+            O.bind("bRight", () => O.map(bDefParentParent, (d) => d.right)),
+            O.exists(({ aRight, bRight }) => AST.isNodeEqual(aRight, bRight)),
+          );
+        }
+        default: {
+          return O.isSome(aVar) && O.isSome(bVar) && aVar.value === bVar.value;
+        }
       }
     }
+    case a.type === AST_NODE_TYPES.MemberExpression
+      && b.type === AST_NODE_TYPES.MemberExpression: {
+      return AST.isNodeEqual(a.property, b.property)
+        && isNodeValueEqual(a.object, b.object, initialScopes);
+    }
+    case a.type === AST_NODE_TYPES.ThisExpression
+      && b.type === AST_NODE_TYPES.ThisExpression: {
+      if (aScope.block === bScope.block) return true;
+      const fa = AST.traverseUp(a, AST.isOneOf(thisBlockTypes));
+      const fb = AST.traverseUp(a, AST.isOneOf(thisBlockTypes));
+      return O.isSome(fa) && O.isSome(fb) && fa.value === fb.value;
+    }
+    default: {
+      const aStatic = getStaticValue(a, aScope);
+      const bStatic = getStaticValue(b, bScope);
+      return !!aStatic && !!bStatic && aStatic.value === bStatic.value;
+    }
   }
-  if (a.type === AST_NODE_TYPES.MemberExpression && b.type === AST_NODE_TYPES.MemberExpression) {
-    return AST.isNodeEqual(a.property, b.property) && isNodeValueEqual(a.object, b.object, initialScopes);
-  }
-  if (a.type === AST_NODE_TYPES.ThisExpression && b.type === AST_NODE_TYPES.ThisExpression) {
-    if (aScope.block === bScope.block) return true;
-    const fa = AST.traverseUp(a, AST.isOneOf(thisBlockTypes));
-    const fb = AST.traverseUp(a, AST.isOneOf(thisBlockTypes));
-    if (O.isSome(fa) && O.isSome(fb)) return fa.value === fb.value;
-    return false;
-  }
-  return false;
 }
