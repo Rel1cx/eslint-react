75afccbeaa7ef20748e869536d99d435282b330e
Eva1ent <rel1cx@proton.me>
2024-12-08 10:25:20 +0800

    refactor(plugins/eslint-plugin): remove 'prefer-read-only-props' from presets

--- packages/plugins/eslint-plugin/src/configs/recommended-type-checked.ts
+++ packages/plugins/eslint-plugin/src/configs/recommended-type-checked.ts
@@ -7,7 +7,7 @@ export const name = "@eslint-react/recommended-type-checked";
 export const rules = {
   ...recommendedTypeScript.rules,
   "@eslint-react/no-leaked-conditional-rendering": "warn",
-  "@eslint-react/prefer-read-only-props": "warn",
+  // "@eslint-react/prefer-read-only-props": "warn",
 } as const satisfies RulePreset;
 
 export const plugins = {
