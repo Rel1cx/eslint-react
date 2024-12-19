ff63953e3192a4b2679bf6382f6a391ba997c3a4
Eva1ent <rel1cx@proton.me>
2024-12-19 21:46:13 +0800

    refactor(website): minor improvements

--- website/widgets/tweet-cards.tsx
+++ website/widgets/tweet-cards.tsx
@@ -2,7 +2,7 @@ import { Tweet } from "react-tweet";
 
 export function TweetCards({ tweets }: { tweets: string[] }) {
   return (
-    <div className="grid grid-cols-2 gap-x-10 gap-y-0 mt-10">
+    <div className="grid grid-cols-2 gap-x-10 gap-y-0 mt-mt-8">
       {tweets.map((id) => <Tweet key={id} id={id} />)}
     </div>
   );
