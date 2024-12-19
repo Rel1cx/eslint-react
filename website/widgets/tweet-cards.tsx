9504c69b16abd5158ae6f1804e1c0c534414dbe0
Eva1ent <rel1cx@proton.me>
2024-12-19 22:23:20 +0800

    refactor(website): update tweet cards layout for better responsiveness

--- website/widgets/tweet-cards.tsx
+++ website/widgets/tweet-cards.tsx
@@ -2,7 +2,7 @@ import { Tweet } from "react-tweet";
 
 export function TweetCards({ tweets }: { tweets: string[] }) {
   return (
-    <div className="grid grid-cols-2 gap-x-10 gap-y-0 mt-mt-8">
+    <div className="grid grid-cols-1 mt-4 sm:grid-cols-2 sm:gap-x-8 sm:mt-8 justify-center">
       {tweets.map((id) => <Tweet key={id} id={id} />)}
     </div>
   );
