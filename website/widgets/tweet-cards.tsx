b338cab71eff11c3b327d40fc068340019ef48bc
Eva1ent <rel1cx@proton.me>
2024-12-23 06:18:56 +0800

    feat(website): implement chunking of tweets for better layout

--- website/widgets/tweet-cards.tsx
+++ website/widgets/tweet-cards.tsx
@@ -1,9 +1,21 @@
+import { useMemo } from "react";
 import { Tweet } from "react-tweet";
 
+import { chunk } from "#/lib/chunk";
+
 export function TweetCards({ tweets }: { tweets: string[] }) {
+  const chunkedTweets = useMemo(() => chunk(tweets, 2), [tweets]);
   return (
-    <div className="grid grid-cols-1 mt-4 sm:grid-cols-2 sm:gap-x-8 sm:mt-8 justify-center">
-      {tweets.map((id) => <Tweet key={id} id={id} />)}
+    <div className="grid grid-cols-1 md:grid-cols-2 sm:gap-x-8 sm:mt-8">
+      {chunkedTweets.map((chunk) => (
+        <div key={chunk.join("_")} className="grid justify-center">
+          {chunk.map((id) => (
+            <div key={id} className="h-fit max-w-full">
+              <Tweet id={id} />
+            </div>
+          ))}
+        </div>
+      ))}
     </div>
   );
 }
