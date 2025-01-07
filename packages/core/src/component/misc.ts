a4a97328e14dfc1bca6a63e1736de689e6c52539
rEl1cx <rel1cx@proton.me>
2025-01-07 08:25:02 +0800

    fix(core): fix component name utils

--- packages/core/src/component/misc.ts
+++ packages/core/src/component/misc.ts
@@ -1,18 +1,18 @@
 import type * as AST from "@eslint-react/ast";
-import { F, O } from "@eslint-react/eff";
+import { O } from "@eslint-react/eff";
 import type { RuleContext } from "@eslint-react/types";
 
 import { getFunctionComponentIdentifier } from "./component-id";
 import { isComponentName } from "./component-name";
 
 export function hasNoneOrValidComponentName(node: AST.TSESTreeFunction, context: RuleContext) {
-  return F.pipe(
-    getFunctionComponentIdentifier(node, context),
-    O.exists(id => {
-      const name = Array.isArray(id)
-        ? id.at(-1)?.name
-        : id.name;
-      return !!name && isComponentName(name);
-    }),
-  );
+  const mbIdentifier = getFunctionComponentIdentifier(node, context);
+  if (O.isNone(mbIdentifier)) {
+    return true;
+  }
+  const id = mbIdentifier.value;
+  const name = Array.isArray(id)
+    ? id.at(-1)?.name
+    : id.name;
+  return !!name && isComponentName(name);
 }
