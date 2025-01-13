01f25309c47cf32eb7876834db66656a99b59bf3
Rel1cx <rel1cx@proton.me>
2025-01-13 12:53:12 +0800

    refactor(scripts): switch from async file reading to synchronous for ignores and version scripts

--- scripts/ignores.ts
+++ scripts/ignores.ts
@@ -12,15 +12,12 @@
 //   Effect.map(Array.filter(Str.isNonEmpty)),
 // );
 
-import fs from "fs/promises";
+import fs from "node:fs";
 
-export const ignores = await fs
-  .readFile(".gitignore", "utf-8")
-  .then((v) => {
-    return v
-      .split("\n")
-      .map((v) => v.trim())
-      .filter((v) => !v.startsWith("#") && !v.startsWith("!"))
-      .map((v) => v.replace(/^\//, ""))
-      .filter((v) => v !== "");
-  });
+export const ignores = fs
+  .readFileSync(".gitignore", "utf-8")
+  .split("\n")
+  .map((v) => v.trim())
+  .filter((v) => !v.startsWith("#") && !v.startsWith("!"))
+  .map((v) => v.replace(/^\//, ""))
+  .filter((v) => v !== "");
--- scripts/version.ts
+++ scripts/version.ts
@@ -1,13 +1,6 @@
-import fs from "fs/promises";
+import fs from "node:fs";
 
-import { isVersion } from "./libs";
-
-export const version = await fs.readFile("VERSION", "utf-8")
-  .then((v) => {
-    const trimmed = v.trim();
-    const version = trimmed.replace("v", "");
-    if (!isVersion(version)) {
-      throw new Error("Invalid version format");
-    }
-    return version;
-  });
+export const version = fs
+  .readFileSync("VERSION", "utf-8")
+  .trim()
+  .replace("v", "");
