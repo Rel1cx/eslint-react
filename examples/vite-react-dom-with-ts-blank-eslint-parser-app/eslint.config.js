945ca60b720eb41927d462f3cfcb27f7b590b508
Rel1cx <rel1cx@proton.me>
2025-01-25 03:02:52 +0800

    feat(examples): add new 'vite-react-dom-with-ts-blank-eslint-parser-app' example

---
+++ examples/vite-react-dom-with-ts-blank-eslint-parser-app/eslint.config.js
@@ -0,0 +1,73 @@
+// @ts-check
+import eslintJs from "@eslint/js";
+import eslintReact from "@eslint-react/eslint-plugin";
+import eslintPluginReactHooks from "eslint-plugin-react-hooks";
+import eslintPluginReactRefresh from "eslint-plugin-react-refresh";
+import globals from "globals";
+import * as tsBlankEslintParser from "ts-blank-eslint-parser";
+
+import TSCONFIG from "./tsconfig.json" with { type: "json" };
+import TSCONFIG_NODE from "./tsconfig.node.json" with { type: "json" };
+
+const GLOB_TS = ["**/*.ts", "**/*.tsx"];
+
+export default [
+  // base configuration for browser environment source files
+  {
+    files: TSCONFIG.include,
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+      parser: tsBlankEslintParser,
+      parserOptions: {
+        jsxPragma: "React",
+        sourceType: "module",
+      },
+    },
+    rules: {
+      ...eslintJs.configs.recommended.rules,
+    },
+  },
+  // base configuration for node environment source files (*.config.js, etc.)
+  {
+    files: TSCONFIG_NODE.include,
+    ignores: TSCONFIG_NODE.exclude,
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+      parser: tsBlankEslintParser,
+      parserOptions: {
+        sourceType: "module",
+      },
+    },
+    rules: {
+      ...eslintJs.configs.recommended.rules,
+      "no-console": "off",
+    },
+  },
+  // React configuration
+  {
+    files: TSCONFIG.include,
+    ...eslintReact.configs.recommended,
+  },
+  // React Hooks configuration
+  {
+    files: TSCONFIG.include,
+    plugins: {
+      "react-hooks": eslintPluginReactHooks,
+    },
+    rules: eslintPluginReactHooks.configs.recommended.rules,
+  },
+  // React Refresh configuration
+  {
+    files: TSCONFIG.include,
+    plugins: {
+      "react-refresh": eslintPluginReactRefresh,
+    },
+    rules: {
+      "react-refresh/only-export-components": "warn",
+    },
+  },
+];
