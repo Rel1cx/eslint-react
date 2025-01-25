48f2cf8458940202390d23dbd026ed5486298f12
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2025-01-26 07:50:32 +0800

    feat(no-useless-fragment): auto fix support, closes #899 (#926)
    Co-authored-by: Rel1cx <rel1cx@proton.me>

--- packages/plugins/eslint-plugin-react-x/src/rules/no-useless-fragment.ts
+++ packages/plugins/eslint-plugin-react-x/src/rules/no-useless-fragment.ts
@@ -3,6 +3,7 @@ import * as JSX from "@eslint-react/jsx";
 import type { RuleContext, RuleFeature } from "@eslint-react/shared";
 import { AST_NODE_TYPES as T } from "@typescript-eslint/types";
 import type { TSESTree } from "@typescript-eslint/utils";
+import type { RuleFixer } from "@typescript-eslint/utils/ts-eslint";
 
 import { createRule } from "../utils";
 
@@ -27,11 +28,56 @@ const defaultOptions = [{
   allowExpressions: true,
 }] as const satisfies Options;
 
+function trimLikeReact(text: string) {
+  const leadingSpaces = /^\s*/.exec(text)?.[0] ?? "";
+  const trailingSpaces = /\s*$/.exec(text)?.[0] ?? "";
+
+  const start = leadingSpaces.includes("\n") ? leadingSpaces.length : 0;
+  const end = trailingSpaces.includes("\n") ? text.length - trailingSpaces.length : text.length;
+
+  return text.slice(start, end);
+}
+
 function checkAndReport(
   node: TSESTree.JSXElement | TSESTree.JSXFragment,
   context: RuleContext,
   allowExpressions: boolean,
 ) {
+  function fix(fixer: RuleFixer) {
+    // Not safe to fix fragments without a jsx parent.
+    if (!(node.parent.type === T.JSXElement || node.parent.type === T.JSXFragment)) {
+      // const a = <></>
+      if (node.children.length === 0) {
+        return null;
+      }
+
+      // const a = <>cat {meow}</>
+      if (
+        node.children.some(
+          (child) =>
+            (JSX.isLiteral(child) && !JSX.isWhiteSpace(child))
+            || AST.is(T.JSXExpressionContainer)(child),
+        )
+      ) {
+        return null;
+      }
+    }
+
+    // Not safe to fix `<Eeee><>foo</></Eeee>` because `Eeee` might require its children be a ReactElement.
+    if (JSX.isUserDefinedElement(node.parent)) {
+      return null;
+    }
+
+    const opener = node.type === T.JSXFragment ? node.openingFragment : node.openingElement;
+    const closer = node.type === T.JSXFragment ? node.closingFragment : node.closingElement;
+
+    const childrenText = opener.type === T.JSXOpeningElement && opener.selfClosing
+      ? ""
+      : context.sourceCode.getText().slice(opener.range[1], closer?.range[0]);
+
+    return fixer.replaceText(node, trimLikeReact(childrenText));
+  }
+
   const initialScope = context.sourceCode.getScope(node);
   // return if the fragment is keyed (e.g. <Fragment key={key}>)
   if (JSX.isKeyedElement(node, initialScope)) {
@@ -39,11 +85,11 @@ function checkAndReport(
   }
   // report if the fragment is placed inside a built-in component (e.g. <div><></></div>)
   if (JSX.isBuiltInElement(node.parent)) {
-    context.report({ messageId: "noUselessFragmentInBuiltIn", node });
+    context.report({ messageId: "noUselessFragmentInBuiltIn", node, fix });
   }
   // report and return if the fragment has no children (e.g. <></>)
   if (node.children.length === 0) {
-    context.report({ messageId: "noUselessFragment", node });
+    context.report({ messageId: "noUselessFragment", node, fix });
     return;
   }
   const isChildElement = AST.isOneOf([T.JSXElement, T.JSXFragment])(node.parent);
@@ -58,7 +104,7 @@ function checkAndReport(
     // <Foo><>hello, world</></Foo>
     case !allowExpressions
       && isChildElement: {
-      context.report({ messageId: "noUselessFragment", node });
+      context.report({ messageId: "noUselessFragment", node, fix });
       return;
     }
     case !allowExpressions
@@ -66,7 +112,7 @@ function checkAndReport(
       && node.children.length === 1: {
       // const foo = <>{children}</>;
       // return <>{children}</>;
-      context.report({ messageId: "noUselessFragment", node });
+      context.report({ messageId: "noUselessFragment", node, fix });
       return;
     }
   }
@@ -76,7 +122,7 @@ function checkAndReport(
     case nonPaddingChildren.length === 0:
     case nonPaddingChildren.length === 1
       && firstNonPaddingChild?.type !== T.JSXExpressionContainer: {
-      context.report({ messageId: "noUselessFragment", node });
+      context.report({ messageId: "noUselessFragment", node, fix });
       return;
     }
   }
@@ -90,6 +136,7 @@ export default createRule<Options, MessageID>({
     docs: {
       description: "disallow unnecessary fragments",
     },
+    fixable: "code",
     messages: {
       noUselessFragment: "A fragment contains less than two children is unnecessary.",
       noUselessFragmentInBuiltIn: "A fragment placed inside a built-in component is unnecessary.",
