93b7a5225757dbc1c16e2ebcefe784460ccfa45f
Rel1cx <rel1cx@proton.me>
2025-02-03 20:21:02 +0800

    chore: move rules docs sources to plugins

--- scripts/update-website.ts
+++ scripts/update-website.ts
@@ -1,6 +1,35 @@
-import fs from "node:fs";
+import fs from "node:fs/promises";
+import path from "node:path";
 
-const changelog = fs.readFileSync("CHANGELOG.md", "utf-8");
+import { glob } from "./lib";
+
+const docs = glob(["packages/plugins/eslint-plugin-react-*/src/rules/*.md"]);
+
+const [
+  files,
+  // rules,
+] = Array.from(docs).reduce<readonly [[string, string][], [string, string][]]>(
+  ([files, rules], doc) => {
+    const catename = /^packages\/plugins\/eslint-plugin-react-([^/]+)/u.exec(doc)?.[1] ?? "";
+    const basename = path.parse(path.basename(doc)).name;
+    const isPluginX = catename === "x";
+    const name = isPluginX
+      ? basename
+      : `${catename}-${basename}`;
+    const title = isPluginX
+      ? basename
+      : `${catename}/${basename}`;
+    const dest = path.join("website", "content", "docs", "rules", `${name}.md`);
+    return [[...files, [doc, dest]], [...rules, [name, title]]] as const;
+  },
+  [[], []],
+);
+
+await Promise.all(files.map(async ([src, dest]) => fs.copyFile(src, dest)));
+
+// fs.writeFileSync(path.join("website", "content", "docs", "rules", "data.json"), JSON.stringify(rules, null, 2));
+
+const changelog = await fs.readFile("CHANGELOG.md", "utf-8");
 
 const changelogWithFrontmatter = [
   "---",
@@ -10,4 +39,4 @@ const changelogWithFrontmatter = [
   changelog,
 ].join("\n");
 
-fs.writeFileSync("website/content/docs/changelog.md", changelogWithFrontmatter);
+await fs.writeFile("website/content/docs/changelog.md", changelogWithFrontmatter);
