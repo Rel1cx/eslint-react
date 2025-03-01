ed1486bea60fdff0de2081354dde94a3554fcf45
Rel1cx <rel1cx@proton.me>
2025-03-01 18:06:09 +0800

    fix(docs): update presets documentation formatting build(website): add 'serverExternalPackages' configuration

--- apps/website/next.config.mjs
+++ apps/website/next.config.mjs
@@ -5,6 +5,10 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  serverExternalPackages: [
+    "typescript",
+    "twoslash",
+  ],
   redirects() {
     return [
       {
