06387c80440dd9613a88e7e545600b6cd91d8d5d
REL1CX <rel1cx@proton.me>
2025-03-02 10:41:54 +0800

    feat: add 'naming-convention/use-state' and 'naming-convention/context-name' to recommended presets (#956)

--- packages/plugins/eslint-plugin-react-naming-convention/src/configs/recommended.ts
+++ packages/plugins/eslint-plugin-react-naming-convention/src/configs/recommended.ts
@@ -2,4 +2,7 @@ import type { RulePreset } from "@eslint-react/shared";
 
 export const name = "react-naming-convention/recommended";
 
-export const rules = {} as const satisfies RulePreset;
+export const rules = {
+  "react-naming-convention/context-name": "warn",
+  "react-naming-convention/use-state": "warn",
+} as const satisfies RulePreset;
