b7020a0d22be9d8be7c575d654b9980f76bb5313
Rel1cx <rel1cx@proton.me>
2025-03-23 09:36:24 +0800

    chore: remove unneeded declare module

--- examples/next-app/eslint.config.d.ts
+++ examples/next-app/eslint.config.d.ts
@@ -1,2 +1,1 @@
 declare module "@eslint/js";
-declare module "eslint-plugin-react-hooks";
--- examples/vite-react-dom-app/eslint.config.d.ts
+++ examples/vite-react-dom-app/eslint.config.d.ts
@@ -1,3 +1,1 @@
 declare module "@eslint/js";
-declare module "eslint-plugin-react-hooks";
-declare module "eslint-plugin-react-refresh";
