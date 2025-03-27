9d9185db88d5da93520b425210ee0002ab0c39bb
Rel1cx <rel1cx@proton.me>
2025-03-27 18:43:47 +0800

    refactor(website): switch back to docs layout

--- apps/website/app/docs/layout.tsx
+++ apps/website/app/docs/layout.tsx
@@ -1,35 +1,35 @@
 // Docs layout
-// import type { ReactNode } from "react";
-// import { baseOptions } from "#/app/layout.config";
-
-// import { source } from "#/lib/source";
-// import { DocsLayout } from "fumadocs-ui/layouts/docs";
-
-// export default function Layout({ children }: { children: ReactNode }) {
-//   return (
-//     <DocsLayout tree={source.pageTree} {...baseOptions}>
-//       {children}
-//     </DocsLayout>
-//   );
-// }
-
-// Notebook layout
 import type { ReactNode } from "react";
 import { baseOptions } from "#/app/layout.config";
+
 import { source } from "#/lib/source";
-import { DocsLayout } from "fumadocs-ui/layouts/notebook";
+import { DocsLayout } from "fumadocs-ui/layouts/docs";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <DocsLayout
-      {...baseOptions}
-      // the position of navbar
-      nav={{ ...baseOptions.nav, mode: "top" }}
-      // the position of Sidebar Tabs
-      tabMode="navbar"
-      tree={source.pageTree}
-    >
+    <DocsLayout tree={source.pageTree} {...baseOptions}>
       {children}
     </DocsLayout>
   );
 }
+
+// Notebook layout
+// import type { ReactNode } from "react";
+// import { baseOptions } from "#/app/layout.config";
+// import { source } from "#/lib/source";
+// import { DocsLayout } from "fumadocs-ui/layouts/notebook";
+
+// export default function Layout({ children }: { children: ReactNode }) {
+//   return (
+//     <DocsLayout
+//       {...baseOptions}
+//       // the position of navbar
+//       nav={{ ...baseOptions.nav, mode: "top" }}
+//       // the position of Sidebar Tabs
+//       tabMode="navbar"
+//       tree={source.pageTree}
+//     >
+//       {children}
+//     </DocsLayout>
+//   );
+// }
