c7fd8614a04fc6c2179fbf7a3c22d8bbf758726c
Rel1cx <rel1cx@proton.me>
2025-03-27 11:36:56 +0800

    refactor(website): update layout component to use notebook layout and adjust navbar positioning

--- apps/website/app/docs/layout.tsx
+++ apps/website/app/docs/layout.tsx
@@ -1,12 +1,34 @@
+// Docs layout
+// import type { ReactNode } from "react";
+// import { baseOptions } from "#/app/layout.config";
+
+// import { source } from "#/lib/source";
+// import { DocsLayout } from "fumadocs-ui/layouts/docs";
+
+// export default function Layout({ children }: { children: ReactNode }) {
+//   return (
+//     <DocsLayout tree={source.pageTree} {...baseOptions}>
+//       {children}
+//     </DocsLayout>
+//   );
+// }
+
+// Notebook layout
 import type { ReactNode } from "react";
 import { baseOptions } from "#/app/layout.config";
-
 import { source } from "#/lib/source";
-import { DocsLayout } from "fumadocs-ui/layouts/docs";
+import { DocsLayout } from "fumadocs-ui/layouts/notebook";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <DocsLayout tree={source.pageTree} {...baseOptions}>
+    <DocsLayout
+      {...baseOptions}
+      // the position of navbar
+      nav={{ ...baseOptions.nav, mode: "top" }}
+      // the position of Sidebar Tabs
+      tabMode="navbar"
+      tree={source.pageTree}
+    >
       {children}
     </DocsLayout>
   );
