6ae2cc766e6a8564a299b42b9c860482cd6cc2c6
REL1CX <rel1cx@proton.me>
2025-03-31 00:15:49 +0800

    feat: enable `context-name` rule in recommended presets (#1024)

--- packages/plugins/eslint-plugin/src/configs/recommended.ts
+++ packages/plugins/eslint-plugin/src/configs/recommended.ts
@@ -13,9 +13,13 @@ export const rules = {
   ...core.rules,
   ...dom.rules,
   ...webApi.rules,
+
   "@eslint-react/hooks-extra/no-direct-set-state-in-use-effect": "warn",
   "@eslint-react/hooks-extra/no-unnecessary-use-prefix": "warn",
   "@eslint-react/hooks-extra/prefer-use-state-lazy-initialization": "warn",
+
+  "@eslint-react/naming-convention/context-name": "warn",
+  // "@eslint-react/naming-convention/use-state": "warn",
 } as const satisfies RulePreset;
 
 export const plugins = {
