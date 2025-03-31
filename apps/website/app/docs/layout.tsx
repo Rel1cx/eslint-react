f6f1646c6bc393cd2b99abd5037eb9d2802b28a4
Rel1cx <rel1cx@proton.me>
2025-04-01 06:37:10 +0800

    fix(website): fixed duplicated items in sidebar

--- apps/website/app/docs/layout.tsx
+++ apps/website/app/docs/layout.tsx
@@ -1,36 +1,39 @@
 // Docs layout
-// import type { ReactNode } from "react";
-// import { baseOptions } from "#/app/layout.config";
-
-// import { source } from "#/lib/source";
-// import { DocsLayout } from "fumadocs-ui/layouts/docs";
-
-// export default function Layout({ children }: { children: ReactNode }) {
-//   return (
-//     <DocsLayout {...baseOptions} tree={source.pageTree}>
-//       {children}
-//     </DocsLayout>
-//   );
-// }
-
-// Notebook layout
 import type { ReactNode } from "react";
 import { baseOptions } from "#/app/layout.config";
+
 import { source } from "#/lib/source";
-import { DocsLayout } from "fumadocs-ui/layouts/notebook";
+import { DocsLayout } from "fumadocs-ui/layouts/docs";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <DocsLayout
       {...baseOptions}
-      links={baseOptions.links?.slice(1, -1) ?? []}
-      // the position of navbar
-      nav={{ ...baseOptions.nav, mode: "top" }}
-      // the position of Sidebar Tabs
-      tabMode="navbar"
       tree={source.pageTree}
     >
       {children}
     </DocsLayout>
   );
 }
+
+// Notebook layout
+// import type { ReactNode } from "react";
+// import { baseOptions } from "#/app/layout.config";
+// import { source } from "#/lib/source";
+// import { DocsLayout } from "fumadocs-ui/layouts/notebook";
+
+// export default function Layout({ children }: { children: ReactNode }) {
+//   return (
+//     <DocsLayout
+//       {...baseOptions}
+//       links={baseOptions.links?.slice(1, -1) ?? []}
+//       // the position of navbar
+//       nav={{ ...baseOptions.nav, mode: "top" }}
+//       // the position of Sidebar Tabs
+//       tabMode="navbar"
+//       tree={source.pageTree}
+//     >
+//       {children}
+//     </DocsLayout>
+//   );
+// }
--- apps/website/app/layout.config.tsx
+++ apps/website/app/layout.config.tsx
@@ -13,48 +13,7 @@ import React from "react";
  */
 export const baseOptions: BaseLayoutProps = {
   githubUrl: "https://github.com/Rel1cx/eslint-react",
-  links: [
-    {
-      active: "nested-url",
-      text: "Getting Started",
-      url: "/docs/getting-started",
-    },
-    {
-      active: "nested-url",
-      text: "Rules",
-      url: "/docs/rules",
-    },
-    {
-      active: "nested-url",
-      text: "Presets",
-      url: "/docs/presets",
-    },
-    {
-      active: "nested-url",
-      text: "Contributing",
-      url: "/docs/contributing",
-    },
-    {
-      active: "nested-url",
-      text: "Community",
-      url: "/docs/community",
-    },
-    {
-      active: "nested-url",
-      text: "Changelog",
-      url: "/docs/changelog",
-    },
-    {
-      active: "nested-url",
-      text: "Roadmap",
-      url: "/docs/roadmap",
-    },
-    {
-      active: "nested-url",
-      text: "FAQ",
-      url: "/FAQ",
-    },
-  ],
+  links: [],
   nav: {
     title: (
       <div className="flex items-between gap-2.5 min-w-max">
