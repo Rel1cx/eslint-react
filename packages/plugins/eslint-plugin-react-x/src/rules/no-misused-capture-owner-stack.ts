b75cc64443cfacec9acca5246f7c78f69cb63825
Rel1cx <rel1cx@proton.me>
2025-04-11 12:26:00 +0800

    docs: set `react-x/no-misused-capture-owner-stack` rule to experimental

--- packages/plugins/eslint-plugin-react-x/src/rules/no-misused-capture-owner-stack.ts
+++ packages/plugins/eslint-plugin-react-x/src/rules/no-misused-capture-owner-stack.ts
@@ -9,7 +9,9 @@ import { createRule } from "../utils";
 
 export const RULE_NAME = "no-misused-capture-owner-stack";
 
-export const RULE_FEATURES = [] as const satisfies RuleFeature[];
+export const RULE_FEATURES = [
+  "EXP",
+] as const satisfies RuleFeature[];
 
 export type MessageID =
   | "useNamespaceImport"
