1cb7049520669dd6ef936b8c65de697cee641a70
REL1CX <rel1cx@proton.me>
2025-04-30 03:36:02 +0800

    refactor: code optimizations (#1098)

--- packages/plugins/eslint-plugin-react-x/src/utils/index.ts
+++ packages/plugins/eslint-plugin-react-x/src/utils/index.ts
@@ -1,6 +1,2 @@
 export * from "./constants";
 export * from "./create-rule";
-export * from "./is-from-hook-call";
-export * from "./is-from-use-state-call";
-export * from "./is-set-function-call";
-export * from "./is-variable-declarator-from-hook-call";
