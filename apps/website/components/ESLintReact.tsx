322bcac9a98103495754cdc0d937d2cbe72bd90c
REL1CX <rel1cx@proton.me>
2025-05-02 16:43:27 +0800

    feat(website): add background effect to homepage

--- apps/website/components/ESLintReact.tsx
+++ apps/website/components/ESLintReact.tsx
@@ -8,6 +8,9 @@ export function ESLintReact() {
     <div className="flex flex-col items-center gap-4 m-0 mx-auto p-8 w-fit">
       <Image alt="logo" height="150" quality={100} src={logo as StaticImageData} width="150" />
       <span className="text-2xl">ESLint React</span>
+      <p className="text-center text-gray-400">
+        A series of composable ESLint rules for React and friends.
+      </p>
     </div>
   );
 }
