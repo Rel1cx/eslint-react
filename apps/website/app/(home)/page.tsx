9bab1c477eb06c37bafdbfc3dff4b7a6020b3971
Rel1cx <rel1cx@proton.me>
2025-05-02 23:43:30 +0800

    feat(website): minor improvements

--- apps/website/app/(home)/page.tsx
+++ apps/website/app/(home)/page.tsx
@@ -25,7 +25,7 @@ export default function HomePage() {
         <Cards className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
           {features.map(([title, description, Icon]) => (
             <Card
-              className="mix-blend-luminosity"
+              className="mix-blend-luminosity shadow-none"
               description={description}
               icon={<Icon />}
               key={title}
