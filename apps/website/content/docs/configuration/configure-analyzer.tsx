5228468a99e80a9f4c8e24ae1bbaacc613b8b7b6
Rel1cx <rel1cx@proton.me>
2025-05-14 22:45:45 +0800

    docs: minor improvements

--- apps/website/content/docs/configuration/configure-analyzer.tsx
+++ apps/website/content/docs/configuration/configure-analyzer.tsx
@@ -5,81 +5,77 @@ import { TypeTable } from "fumadocs-ui/components/type-table";
 
 import { Link } from "next-view-transitions";
 
+const properties = {
+  additionalHooks: {
+    type: "Record<ReactBuiltInHookName, string[]>",
+    description: <Link href="#additionalhooks">An object of aliases for React built-in Hooks ⤵</Link>,
+    default: "{}",
+    typeDescription: (
+      <CodeBlock
+        code={dedent`
+          type ReactBuiltInHookName =
+            | "use"
+            | "useActionState"
+            | "useCallback"
+            | "useContext"
+            | "useDebugValue"
+            | "useDeferredValue"
+            | "useEffect"
+            | "useFormStatus"
+            | "useId"
+            | "useImperativeHandle"
+            | "useInsertionEffect"
+            | "useLayoutEffect"
+            | "useMemo"
+            | "useOptimistic"
+            | "useReducer"
+            | "useRef"
+            | "useState"
+            | "useSyncExternalStore"
+            | "useTransition";
+        `}
+        lang="ts"
+      />
+    ),
+  },
+  additionalComponents: {
+    type: "CustomComponent[]",
+    description: <Link href="#additionalcomponents">An array of components and its attributes mapping ⤵</Link>,
+    default: "[]",
+    typeDescription: (
+      <CodeBlock
+        code={dedent`
+          type CustomComponent = {
+            name: string;
+            as?: string;
+            attributes?: {
+              name: string;
+              as?: string;
+              defaultValue?: string;
+            }[]
+          }
+        `}
+        lang="ts"
+      />
+    ),
+  },
+  polymorphicPropName: {
+    type: "string",
+    description: <Link href="#polymorphicpropname">The prop your code uses to create polymorphic components ⤵</Link>,
+    default: '"as"',
+  },
+  importSource: {
+    type: "string",
+    description: <Link href="#importsource">The source where React is imported from ⤵</Link>,
+    default: '"react"',
+  },
+  version: {
+    type: "string",
+    description: <Link href="#version">React version to perform the analysis ⤵</Link>,
+    default: '"detect"',
+  },
+};
+
 export function AnalyzerPropertyTypeTable() {
-  return (
-    <TypeTable
-      type={{
-        version: {
-          type: "string",
-          description: <Link href="#version">React version to perform the analysis ⤵</Link>,
-          default: '"detect"',
-        },
-        importSource: {
-          type: "string",
-          description: <Link href="#importsource">The source where React is imported from ⤵</Link>,
-          default: '"react"',
-        },
-        polymorphicPropName: {
-          type: "string",
-          description: (
-            <Link href="#polymorphicpropname">The prop your code uses to create polymorphic components ⤵</Link>
-          ),
-          default: '"as"',
-        },
-        additionalComponents: {
-          type: "CustomComponent[]",
-          description: <Link href="#additionalcomponents">An array of components and its attributes mapping ⤵</Link>,
-          default: "[]",
-          typeDescription: (
-            <CodeBlock
-              code={dedent`
-                type CustomComponent = {
-                  name: string;
-                  as?: string;
-                  attributes?: {
-                    name: string;
-                    as?: string;
-                    defaultValue?: string;
-                  }[]
-                }
-              `}
-              lang="ts"
-            />
-          ),
-        },
-        additionalHooks: {
-          type: "Record<ReactBuiltInHookName, string[]>",
-          description: <Link href="#additionalhooks">An object of aliases for React built-in Hooks ⤵</Link>,
-          default: "{}",
-          typeDescription: (
-            <CodeBlock
-              code={dedent`
-                type ReactBuiltInHookName =
-                  | "use"
-                  | "useActionState"
-                  | "useCallback"
-                  | "useContext"
-                  | "useDebugValue"
-                  | "useDeferredValue"
-                  | "useEffect"
-                  | "useFormStatus"
-                  | "useId"
-                  | "useImperativeHandle"
-                  | "useInsertionEffect"
-                  | "useLayoutEffect"
-                  | "useMemo"
-                  | "useOptimistic"
-                  | "useReducer"
-                  | "useRef"
-                  | "useState"
-                  | "useSyncExternalStore"
-                  | "useTransition";
-              `}
-              lang="ts"
-            />
-          ),
-        },
-      }}
-    />
-  );
+  return <TypeTable type={properties} />;
 }
