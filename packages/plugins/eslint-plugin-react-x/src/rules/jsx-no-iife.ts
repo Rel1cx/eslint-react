76a663e493c6f634597e1706910b3221db44d866
Rel1cx <rel1cx@proton.me>
2025-05-30 03:58:37 +0800

    feat(react-x): add 'jsx-no-iife' rule, closes #1112

---
+++ packages/plugins/eslint-plugin-react-x/src/rules/jsx-no-iife.ts
@@ -0,0 +1,51 @@
+import type * as AST from "@eslint-react/ast";
+import type { RuleContext, RuleFeature } from "@eslint-react/kit";
+
+import type { RuleListener } from "@typescript-eslint/utils/ts-eslint";
+import type { CamelCase } from "string-ts";
+import { AST_NODE_TYPES as T } from "@typescript-eslint/types";
+import { createRule } from "../utils";
+
+export const RULE_NAME = "jsx-no-iife";
+
+export const RULE_FEATURES = [] as const satisfies RuleFeature[];
+
+export type MessageID = CamelCase<typeof RULE_NAME>;
+
+export default createRule<[], MessageID>({
+  meta: {
+    type: "problem",
+    docs: {
+      description: "Disallows 'IIFE' in JSX elements.",
+      [Symbol.for("rule_features")]: RULE_FEATURES,
+    },
+    messages: {
+      jsxNoIife: "",
+    },
+    schema: [],
+  },
+  name: RULE_NAME,
+  create,
+  defaultOptions: [],
+});
+
+export function create(context: RuleContext<MessageID, []>): RuleListener {
+  return {
+    "JSXElement :function"(node: AST.TSESTreeFunction) {
+      if (node.parent.type === T.CallExpression && node.parent.callee === node) {
+        context.report({
+          messageId: "jsxNoIife",
+          node: node.parent,
+        });
+      }
+    },
+    "JSXFragment :function"(node: AST.TSESTreeFunction) {
+      if (node.parent.type === T.CallExpression && node.parent.callee === node) {
+        context.report({
+          messageId: "jsxNoIife",
+          node: node.parent,
+        });
+      }
+    },
+  };
+}
