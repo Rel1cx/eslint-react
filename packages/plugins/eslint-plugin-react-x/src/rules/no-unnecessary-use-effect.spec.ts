8afc014e092d0d50d913de86f505a04feab0d402
Rel1cx <rel1cx@proton.me>
2025-06-02 14:31:46 +0800

    test: add a test case for ProfileScreen component using no-unnecessary-use-effect rule

--- packages/plugins/eslint-plugin-react-x/src/rules/no-unnecessary-use-effect.spec.ts
+++ packages/plugins/eslint-plugin-react-x/src/rules/no-unnecessary-use-effect.spec.ts
@@ -945,5 +945,27 @@ ruleTester.run(RULE_NAME, rule, {
         }, [])
       }
     `,
+    tsx`
+      function ProfileScreen({ navigation, route }) {
+             const [value, onChangeText] = React.useState(route.params.title);
+
+             React.useEffect(() => {
+               navigation.setOptions({
+                 title: value === '' ? 'No title' : value,
+               });
+             }, [navigation, value]);
+
+             return (
+               <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                 <TextInput
+                   style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+                   onChangeText={onChangeText}
+                   value={value}
+                 />
+                 <Button title="Go back" onPress={() => navigation.goBack()} />
+               </View>
+             );
+           }
+    `,
   ],
 });
