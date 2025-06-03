c2bb0a27bb6811600fd1a19fef0f1c03dfadb0b2
REL1CX <rel1cx@proton.me>
2025-06-03 19:06:31 +0800

    fix: fixed `use-no-direct-set-state-in-use-effect` deferred setState calls detection, closes #1117 (#1119)

--- packages/plugins/eslint-plugin-react-hooks-extra/src/utils/is-function-of-immediately-invoked.ts
+++
@@ -1,8 +0,0 @@
-import type * as AST from "@eslint-react/ast";
-import { AST_NODE_TYPES as T } from "@typescript-eslint/types";
-
-export function isFunctionOfImmediatelyInvoked(node: AST.TSESTreeFunction): boolean {
-  return node.type !== T.FunctionDeclaration
-    && node.parent.type === T.CallExpression
-    && node.parent.callee === node;
-}
