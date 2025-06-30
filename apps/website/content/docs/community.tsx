d4a0243bab577d4378f802f69899fb80ac54823c
Rel1cx <rel1cx@proton.me>
2025-06-30 10:13:45 +0800

    docs: add 'react-starter-kit' project to community list

--- apps/website/content/docs/community.tsx
+++ apps/website/content/docs/community.tsx
@@ -8,6 +8,7 @@ export const projects = [
   { owner: "flirtual", repo: "flirtual" },
   { owner: "hashintel", repo: "hash" },
   { owner: "kriasoft", repo: "graphql-starter-kit" },
+  { owner: "kriasoft", repo: "react-starter-kit" },
   { owner: "mgilangjanuar", repo: "buntal" },
   { owner: "npmgraph", repo: "npmgraph" },
   { owner: "offlegacy", repo: "event-tracker" },
