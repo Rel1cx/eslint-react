b9bda2c14e570c512894a5dce44c1f45c3fd3a9d
Rel1cx <rel1cx@proton.me>
2025-07-01 04:48:16 +0800

    chore: add 'letstri/permix' project to community adoption list

--- apps/website/lib/get-device-pixel-ratio.ts
+++
@@ -1,5 +0,0 @@
-export function getDevicePixelRatio(fallback = 2, precision = 3) {
-  if (typeof window === "undefined") return fallback;
-  const factor = Math.pow(10, precision);
-  return Math.round(window.devicePixelRatio * factor) / factor;
-}
