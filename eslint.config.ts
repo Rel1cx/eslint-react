9e6acc05728735b88b65f2fd7ecbdcce3eb22180
Rel1cx <rel1cx@proton.me>
2025-09-05 08:31:30 +0800

    chore(local/configs): replace `tseslint.config` with `defineConfig`

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,43 +1,40 @@
 import url from "node:url";
 
 import markdown from "@eslint/markdown";
-import * as configs from "@local/configs/eslint";
+import {
+  disableTypeChecked,
+  GLOB_CONFIGS,
+  GLOB_IGNORES,
+  GLOB_MD,
+  GLOB_SCRIPTS,
+  GLOB_TESTS,
+  GLOB_TS,
+  strictTypeChecked,
+} from "@local/configs/eslint";
 import pluginLocal from "@local/eslint-plugin-local";
 import gitIgnores from "eslint-config-flat-gitignore";
-import * as pluginFastImport from "eslint-plugin-fast-import";
+import { recommended as fastImportRecommended } from "eslint-plugin-fast-import";
 import pluginVitest from "eslint-plugin-vitest";
-import { globalIgnores } from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
 import tseslint from "typescript-eslint";
 
 const dirname = url.fileURLToPath(new URL(".", import.meta.url));
-
-const GLOB_TS = ["*.{ts,tsx,cts,mts}", "**/*.{ts,tsx,cts,mts}"];
-const GLOB_MD = ["*.md", "**/*.md"];
-const GLOB_TEST = [
-  "**/*.spec.{ts,tsx,cts,mts}",
-  "**/*.test.{ts,tsx,cts,mts}",
-  "**/spec.{ts,tsx,cts,mts}",
-  "**/test.{ts,tsx,cts,mts}",
-];
-
-const GLOB_CONFIG = ["*.config.{ts,tsx,cts,mts}", "**/*.config.{ts,tsx,cts,mts}"];
-const GLOB_SCRIPT = ["scripts/**/*.{ts,cts,mts}"];
-const GLOB_IGNORES = [
-  "apps",
-  "docs",
-  "test",
-  "examples",
-  "**/*.d.ts",
-];
 
 const packagesTsConfigs = [
   "packages/*/tsconfig.json",
   "packages/*/*/tsconfig.json",
 ];
 
-export default tseslint.config(
+export default defineConfig([
   gitIgnores(),
-  globalIgnores(GLOB_IGNORES),
+  globalIgnores([
+    ...GLOB_IGNORES,
+    "apps",
+    "docs",
+    "test",
+    "examples",
+    "**/*.d.ts",
+  ]),
   {
     extends: [
       markdown.configs.recommended,
@@ -51,13 +48,14 @@ export default tseslint.config(
     rules: {
       "markdown/no-html": "off",
       "markdown/no-missing-label-refs": "off",
+      "markdown/no-multiple-h1": "off",
     },
   },
   {
     extends: [
-      ...tseslint.configs.strictTypeChecked,
-      configs.typescript,
-      pluginFastImport.recommended({ rootDir: dirname }),
+      tseslint.configs.strictTypeChecked,
+      strictTypeChecked,
+      fastImportRecommended({ rootDir: dirname }),
     ],
     files: GLOB_TS,
     languageOptions: {
@@ -66,7 +64,6 @@ export default tseslint.config(
         project: packagesTsConfigs,
         projectService: true,
         tsconfigRootDir: dirname,
-        // warnOnUnsupportedTypeScriptVersion: false,
       },
     },
     plugins: {
@@ -83,9 +80,9 @@ export default tseslint.config(
   },
   {
     extends: [
-      configs.disableTypeChecked,
+      disableTypeChecked,
     ],
-    files: [...GLOB_SCRIPT, ...GLOB_CONFIG],
+    files: [...GLOB_SCRIPTS, ...GLOB_CONFIGS],
     languageOptions: {
       parserOptions: {
         project: false,
@@ -100,7 +97,7 @@ export default tseslint.config(
     extends: [
       pluginVitest.configs.recommended,
     ],
-    files: GLOB_TEST,
+    files: GLOB_TESTS,
     languageOptions: {
       globals: {
         ...pluginVitest.environments.env.globals,
@@ -119,4 +116,4 @@ export default tseslint.config(
       "local/avoid-multiline-template-expression": "off",
     },
   },
-);
+] as never);
