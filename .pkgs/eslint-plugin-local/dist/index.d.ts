aaa733c89f25e2890a2998ed445741e96dc9c482
Rel1cx <rel1cx@proton.me>
2025-09-10 18:21:33 +0800

    release: 1.53.1-next.1

--- .pkgs/eslint-plugin-local/dist/index.d.ts
+++ .pkgs/eslint-plugin-local/dist/index.d.ts
@@ -1,5 +1,9 @@
+import "@typescript-eslint/utils/eslint-utils";
 import * as _typescript_eslint_utils_ts_eslint0 from "@typescript-eslint/utils/ts-eslint";
 
+//#region src/rules/prefer-eqeq-nullish-comparison.d.ts
+type MessageID = "unexpectedComparison" | "useLooseComparisonSuggestion";
+//#endregion
 //#region src/index.d.ts
 declare const _default: {
   readonly meta: {
@@ -9,7 +13,7 @@ declare const _default: {
   readonly rules: {
     readonly "avoid-multiline-template-expression": _typescript_eslint_utils_ts_eslint0.RuleModule<"avoidMultilineTemplateExpression", [], unknown, _typescript_eslint_utils_ts_eslint0.RuleListener>;
     readonly "no-shadow-underscore": _typescript_eslint_utils_ts_eslint0.RuleModule<"noShadowUnderscore", [], unknown, _typescript_eslint_utils_ts_eslint0.RuleListener>;
-    readonly "prefer-eqeq-nullish-comparison": _typescript_eslint_utils_ts_eslint0.RuleModule<"unexpectedComparison" | "useLooseComparisonSuggestion", [], unknown, _typescript_eslint_utils_ts_eslint0.RuleListener>;
+    readonly "prefer-eqeq-nullish-comparison": _typescript_eslint_utils_ts_eslint0.RuleModule<MessageID, [], unknown, _typescript_eslint_utils_ts_eslint0.RuleListener>;
   };
 };
 //#endregion
