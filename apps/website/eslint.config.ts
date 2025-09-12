a0a088315053f5036c999cbf0e9bb71551cf67b8
Rel1cx <rel1cx@proton.me>
2025-09-13 01:08:46 +0800

    chore: enable pnpm stream log output

--- apps/website/eslint.config.ts
+++ apps/website/eslint.config.ts
@@ -1,9 +1,13 @@
 import react from "@eslint-react/eslint-plugin";
 import markdown from "@eslint/markdown";
-import * as configs from "@local/configs/eslint";
+import { disableTypeChecked, strictTypeChecked } from "@local/configs/eslint";
 import gitignore from "eslint-config-flat-gitignore";
+import { recommended as fastImportRecommended } from "eslint-plugin-fast-import";
 import pluginReactHooks from "eslint-plugin-react-hooks";
 import pluginReactRefresh from "eslint-plugin-react-refresh";
+import { globalIgnores } from "eslint/config";
+import { defineConfig } from "eslint/config";
+import url from "node:url";
 import tseslint from "typescript-eslint";
 
 import TSCONFIG from "./tsconfig.json" with { type: "json" };
@@ -15,8 +19,17 @@ const GLOB_MDX = ["**/*.mdx"];
 const GLOB_APP = ["app/**/*.{js,ts,jsx,tsx}"];
 const GLOB_COMPONENT = ["components/**/*.{js,ts,jsx,tsx}"];
 const GLOB_CONFIG = ["**/*.config.{js,mjs,ts,tsx}"];
+const GLOB_IGNORES = [
+  "test",
+  "**/*.d.ts",
+  "eslint.config.ts",
+];
 
-export default tseslint.config(
+const dirname = url.fileURLToPath(new URL(".", import.meta.url));
+
+export default defineConfig([
+  gitignore(),
+  globalIgnores(GLOB_IGNORES),
   {
     extends: [
       markdown.configs.recommended,
@@ -36,11 +49,14 @@ export default tseslint.config(
   },
   {
     extends: [
-      configs.typescript,
+      strictTypeChecked, // @ts-expect-error - types issue
+      fastImportRecommended({ rootDir: dirname }),
     ],
     files: GLOB_TS,
     rules: {
       "no-restricted-syntax": "off",
+      "fast-import/no-unused-exports": "off",
+      "fast-import/no-unresolved-imports": "off",
     },
   },
   {
@@ -64,6 +80,7 @@ export default tseslint.config(
     rules: {
       ...pluginReactHooks.configs.recommended.rules,
       "@eslint-react/naming-convention/filename": ["error", { rule: "kebab-case" }],
+      "@eslint-react/no-unused-props": "warn",
       "react-refresh/only-export-components": "warn",
     },
   },
@@ -82,7 +99,8 @@ export default tseslint.config(
   },
   {
     extends: [
-      configs.disableTypeChecked,
+      disableTypeChecked,
+      react.configs["disable-type-checked"],
     ],
     files: [...GLOB_JS, ...GLOB_CONFIG],
     rules: {
@@ -90,5 +108,4 @@ export default tseslint.config(
       "no-undef": "off",
     },
   },
-  gitignore(),
-);
+]);
