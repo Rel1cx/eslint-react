4615d4c7ea52e96d388ef2c2f85957ced5851c82
REL1CX <rel1cx@proton.me>
2025-09-22 20:11:39 +0800

    feat: remove eslint legacy config support (#1236)

--- packages/plugins/eslint-plugin-react-debug/src/index.ts
+++ packages/plugins/eslint-plugin-react-debug/src/index.ts
@@ -3,12 +3,11 @@ import { getConfigAdapters } from "@eslint-react/shared";
 import * as allConfig from "./configs/all";
 import { plugin } from "./plugin";
 
-const { toFlatConfig, toLegacyConfig } = getConfigAdapters("react-debug", plugin);
+const { toFlatConfig } = getConfigAdapters("react-debug", plugin);
 
 export default {
   ...plugin,
   configs: {
     ["all"]: toFlatConfig(allConfig),
-    ["all-legacy"]: toLegacyConfig(allConfig),
   },
 };
