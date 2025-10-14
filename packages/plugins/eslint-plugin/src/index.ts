60734365ce32a90adfd77e8819778f313b4fb423
REL1CX <rel1cx@proton.me>
2025-10-14 17:28:52 +0800

    Remove remaining items in the unified plugin used for legacy config compatibility and simplify plugin export (#1277)

--- packages/plugins/eslint-plugin/src/index.ts
+++ packages/plugins/eslint-plugin/src/index.ts
@@ -1,10 +1,6 @@
 import { name, version } from "../package.json";
 
-import type { CompatiblePlugin } from "@eslint-react/shared";
-import reactDom from "eslint-plugin-react-dom";
-import reactHooksExtra from "eslint-plugin-react-hooks-extra";
-import reactNamingConvention from "eslint-plugin-react-naming-convention";
-import reactWebApi from "eslint-plugin-react-web-api";
+import type { CompatibleConfig, CompatiblePlugin } from "@eslint-react/shared";
 import react from "eslint-plugin-react-x";
 
 import * as allConfig from "./configs/all";
@@ -24,90 +20,56 @@ import * as strictTypescriptConfig from "./configs/strict-typescript";
 import * as webApiConfig from "./configs/web-api";
 import * as xConfig from "./configs/x";
 
-import { padKeysLeft } from "./utils";
+type ConfigName =
+  | "all"
+  | "disable-conflict-eslint-plugin-react"
+  | "disable-dom"
+  | "disable-type-checked"
+  | "disable-web-api"
+  | "dom"
+  | "no-deprecated"
+  | "off"
+  | "recommended"
+  | "recommended-type-checked"
+  | "recommended-typescript"
+  | "strict"
+  | "strict-type-checked"
+  | "strict-typescript"
+  | "web-api"
+  | "x";
 
-const plugin: CompatiblePlugin = {
+const plugin: CompatiblePlugin & {
+  /**
+   * For more information about each preset, please refer to the documentation.
+   * @see https://eslint-react.xyz/docs/presets
+   */
+  configs: Record<ConfigName, CompatibleConfig>;
+} = {
   meta: {
     name,
     version,
   },
+  configs: {
+    ["all"]: allConfig,
+    ["disable-conflict-eslint-plugin-react"]: disableConflictEslintPluginReact,
+    ["disable-dom"]: disableDomConfig,
+    ["disable-type-checked"]: disableTypeCheckedConfig,
+    ["disable-web-api"]: disableWebApiConfig,
+    ["dom"]: domConfig,
+    ["no-deprecated"]: noDeprecatedConfig,
+    ["off"]: offConfig,
+    ["recommended"]: recommendedConfig,
+    ["recommended-type-checked"]: recommendedTypeCheckedConfig,
+    ["recommended-typescript"]: recommendedTypeScriptConfig,
+    ["strict"]: strictConfig,
+    ["strict-type-checked"]: strictTypeCheckedConfig,
+    ["strict-typescript"]: strictTypescriptConfig,
+    ["web-api"]: webApiConfig,
+    ["x"]: xConfig,
+  },
   rules: {
     ...react.rules,
-    ...padKeysLeft(reactDom.rules, "dom/"),
-    ...padKeysLeft(reactWebApi.rules, "web-api/"),
-    ...padKeysLeft(reactHooksExtra.rules, "hooks-extra/"),
-    ...padKeysLeft(reactNamingConvention.rules, "naming-convention/"),
   },
 };
 
-export default {
-  ...plugin,
-  configs: {
-    /**
-     * Enable all applicable rules from this plugin
-     */
-    ["all"]: allConfig,
-    /**
-     * Disable rules in `eslint-plugin-react` that conflict with rules in our plugins
-     */
-    ["disable-conflict-eslint-plugin-react"]: disableConflictEslintPluginReact,
-    /**
-     * Disable rules in the `dom` preset
-     */
-    ["disable-dom"]: disableDomConfig,
-    /**
-     * Disable rules that require type information
-     */
-    ["disable-type-checked"]: disableTypeCheckedConfig,
-    /**
-     * Disable rules in the `web-api` preset
-     */
-    ["disable-web-api"]: disableWebApiConfig,
-    /**
-     * Enable rules for `"react-dom"`
-     */
-    ["dom"]: domConfig,
-    /**
-     * Enable all non-deprecated rules from the `x` and `dom` presets
-     * This preset sets the severity of these rules to `"error"`
-     */
-    ["no-deprecated"]: noDeprecatedConfig,
-    /**
-     * Disable all rules in this plugin except for debug rules
-     */
-    ["off"]: offConfig,
-    /**
-     * Enforce rules that are recommended by ESLint React for general purpose React + React DOM projects
-     * This preset includes the `x`, `dom`, and `web-api` presets
-     */
-    ["recommended"]: recommendedConfig,
-    /**
-     * Same as the `recommended-typescript` preset but enables additional rules that require type information
-     */
-    ["recommended-type-checked"]: recommendedTypeCheckedConfig,
-    /**
-     * Same as the `recommended` preset but disables rules that can be enforced by TypeScript
-     */
-    ["recommended-typescript"]: recommendedTypeScriptConfig,
-    /**
-     * More strict version of the `recommended` preset
-     */
-    ["strict"]: strictConfig,
-    /**
-     * Same as the `strict-typescript` preset but enables additional rules that require type information
-     */
-    ["strict-type-checked"]: strictTypeCheckedConfig,
-    /**
-     * Same as the `strict` preset but disables rules that can be enforced by TypeScript
-     */
-    ["strict-typescript"]: strictTypescriptConfig,
-    /**
-     * Enable rules for interacting with Web APIs
-     */
-    ["web-api"]: webApiConfig,
-    /**
-     * Enable rules for `"react"`
-     */
-    ["x"]: xConfig,
-  },
-};
+export default plugin;
