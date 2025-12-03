f0332f893f8f1c6b0236d577955fdc00837e0815
Rel1cx <rel1cx@proton.me>
2025-12-03 17:00:26 +0800

    Update README badges to reflect new build tool version and enhance README update script

--- scripts/update-readme.ts
+++ scripts/update-readme.ts
@@ -4,6 +4,8 @@ import * as Command from "@effect/platform/Command";
 import * as CommandExecutor from "@effect/platform/CommandExecutor";
 import * as FileSystem from "@effect/platform/FileSystem";
 import * as Effect from "effect/Effect";
+import * as Str from "effect/String";
+import { P, match } from "ts-pattern";
 
 const getCurrentBranch = Command.make("git", "branch", "--show-current");
 
@@ -13,21 +15,24 @@ const program = Effect.gen(function*() {
   const branch = yield* ce.string(getCurrentBranch);
   const source = "README.md";
   const destination = "packages/plugins/eslint-plugin/README.md";
-  // Ensure the destination directory exists
-  yield* fs.makeDirectory("packages/plugins/eslint-plugin", { recursive: true });
-
+  const buildToolVersion = match(JSON.parse(yield* fs.readFileString("package.json", "utf8")))
+    .with({ devDependencies: { tsdown: P.select(P.string) } }, Str.replace("^", ""))
+    .otherwise(() => "latest");
   const readmeContent = yield* fs.readFileString(source, "utf8");
-
+  // dprint-ignore
+  const readmeContentNew = readmeContent.replaceAll(/https:\/\/img\.shields\.io\/badge\/built_with-tsdown@.*-000000/g, `https://img.shields.io/badge/built_with-tsdown@${buildToolVersion.replaceAll("-", "--")}-000000`);
   // Convert relative links to absolute links
-  const updatedContent = readmeContent.replace(/\[([^\]]+)\]\(([^)]+)\)/g, (match, text, url) => {
+  const readmeContentAbs = readmeContentNew.replace(/\[([^\]]+)\]\(([^)]+)\)/g, (match, text, url) => {
     if (url.startsWith("http://") || url.startsWith("https://") || url.startsWith("#")) {
       return match; // Leave absolute links unchanged
     }
     const absoluteUrl = `${`https://github.com/Rel1cx/eslint-react/tree/${branch.trim()}`}/${url.replace(/^\.\//, "")}`;
     return `[${text}](${absoluteUrl})`;
   });
-
-  yield* fs.writeFileString(destination, updatedContent);
+  // Ensure the destination directory exists
+  yield* fs.makeDirectory("packages/plugins/eslint-plugin", { recursive: true });
+  yield* fs.writeFileString(source, readmeContentNew);
+  yield* fs.writeFileString(destination, readmeContentAbs);
   yield* Effect.log(`Updated ${destination} from ${source}`);
 });
 
