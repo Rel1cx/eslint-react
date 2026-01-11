2c261b14eb1c7f8c6161c66edd8b6f5cea580ac9
Rel1cx <solarflarex@qq.com>
2026-01-12 05:40:23 +0800

    Remove EXP flags and add warning callouts in some rules

--- packages/plugins/eslint-plugin-react-hooks-extra/src/rules/no-direct-set-state-in-use-effect.ts
+++ packages/plugins/eslint-plugin-react-hooks-extra/src/rules/no-direct-set-state-in-use-effect.ts
@@ -15,9 +15,7 @@ import { createRule, isVariableDeclaratorFromHookCall } from "../utils";
 
 export const RULE_NAME = "no-direct-set-state-in-use-effect";
 
-export const RULE_FEATURES = [
-  "EXP",
-] as const satisfies RuleFeature[];
+export const RULE_FEATURES = [] as const satisfies RuleFeature[];
 
 type MessageID = CamelCase<typeof RULE_NAME>;
 
