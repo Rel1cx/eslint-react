1ec823da43a936c6cc6d7694f91ee893320fb503
Rel1cx <solarflarex@qq.com>
2026-01-27 05:53:33 +0800

    Add SemanticFunction interface

--- packages/core/src/semantic/index.ts
+++ packages/core/src/semantic/index.ts
@@ -1,2 +1,3 @@
-export type * from "./semantic-entry";
-export type * from "./semantic-node";
+export type * from './semantic-entry';
+export type * from './semantic-function';
+export type * from './semantic-node';
---
+++ packages/core/src/semantic/semantic-function.ts
@@ -0,0 +1,28 @@
+import type * as AST from "@eslint-react/ast";
+import type { unit } from "@eslint-react/eff";
+import type { TSESTree } from "@typescript-eslint/types";
+
+import type { SemanticNode } from "../semantic";
+
+/* eslint-disable perfectionist/sort-interfaces */
+export interface SemanticFunction extends SemanticNode {
+  // The identifier of the function
+  id: AST.FunctionID | unit;
+  // The AST node of the function
+  node: AST.TSESTreeFunction;
+  // The name of the function
+  name: string | unit;
+  // The head of the function
+  head: TSESTree.Expression | unit;
+  // The return type of the function
+  type: TSESTree.TSTypeAnnotation | unit;
+  // The body of the function
+  body: TSESTree.BlockStatement | TSESTree.Expression;
+  // The directives of the function (e.g., "use strict", "use client", "use server", etc.)
+  directives: TSESTree.Literal[];
+  // The parameters of the function
+  parameters: TSESTree.Parameter[];
+  // The type parameters of the function
+  typeParameters: TSESTree.TSTypeParameterDeclaration | unit;
+}
+/* eslint-enable perfectionist/sort-interfaces */
