6d4c8f7cd5e1389b2e029bac419118e0412f5a3f
SukkaW <isukkaw@gmail.com>
2026-02-01 21:59:15 +0800

    feat(#1435): implements `no-non-async-server-functions`

---
+++ packages/plugins/eslint-plugin-react-x/src/rules/no-non-async-server-functions.spec.ts
@@ -0,0 +1,198 @@
+import tsx from "dedent";
+
+import { allValid, ruleTester } from "../../../../../test";
+import rule, { RULE_NAME } from "./no-non-async-server-functions";
+
+ruleTester.run(RULE_NAME, rule, {
+  invalid: [
+    {
+      code: tsx`
+        'use server';
+        export function serverFunction() {
+          return 42;
+        }
+      `,
+      errors: [{ messageId: "default" }],
+    },
+    {
+      code: tsx`
+        'use server';
+        export default function serverFunction() {
+          return 42;
+        }
+      `,
+      errors: [{ messageId: "default" }],
+    },
+    {
+      code: tsx`
+        'use server';
+        function serverFunction() {
+          return 42;
+        }
+        export { serverFunction };
+      `,
+      errors: [{ messageId: "default" }],
+    },
+    {
+      code: tsx`
+        'use server';
+        export const serverFunction = () => {
+          return 42;
+        }
+      `,
+      errors: [{ messageId: "default" }],
+    },
+    {
+      code: tsx`
+        'use server';
+        const serverFunction = () => {
+          return 42;
+        }
+        export { serverFunction };
+      `,
+      errors: [{ messageId: "default" }],
+    },
+    {
+      code: tsx`
+        'use server';
+        const serverFunction = () => {
+          return 42;
+        }
+        export default serverFunction;
+      `,
+      errors: [{ messageId: "default" }],
+    },
+    {
+      code: tsx`
+        function serverFunction() {
+          'use server';
+          return 42;
+        }
+      `,
+      errors: [{ messageId: "default" }],
+    },
+    {
+      code: tsx`
+        const serverFunction = () => {
+          'use server';
+          return 42;
+        }
+      `,
+      errors: [{ messageId: "default" }],
+    },
+    {
+      code: tsx`
+        export function Component() {
+          function serverFunction() {
+            'use server';
+            return 42;
+          }
+
+          return <div />;
+        }
+      `,
+      errors: [{ messageId: "default" }],
+    },
+    {
+      code: tsx`
+        export const Component = () => {
+          function serverFunction() {
+            'use server';
+            return 42;
+          }
+
+          return <div />;
+        }
+      `,
+      errors: [{ messageId: "default" }],
+    },
+    {
+      code: tsx`
+        export function Component() {
+          const serverFunction = () => {
+            'use server';
+            return 42;
+          }
+
+          return <div />;
+        }
+      `,
+      errors: [{ messageId: "default" }],
+    },
+    {
+      code: tsx`
+        export const Component = () => {
+          const serverFunction = () => {
+            'use server';
+            return 42;
+          }
+
+          return <div />;
+        }
+      `,
+      errors: [{ messageId: "default" }],
+    },
+  ],
+  valid: [
+    ...allValid,
+    tsx`
+      'use server';
+      export async function serverFunction() {
+        return 42;
+      }
+    `,
+    tsx`
+      'use server';
+      export default async function serverFunction() {
+        return 42;
+      }
+    `,
+    tsx`
+      'use server';
+      export const serverFunction = async () => {
+        return 42;
+      }
+    `,
+    tsx`
+      'use server';
+      const serverFunction = async () => {
+        return 42;
+      }
+      export default serverFunction;
+    `,
+    tsx`
+      export {};
+      async function serverFunction() {
+        'use server';
+        return 42;
+      }
+    `,
+    tsx`
+      export {};
+      const serverFunction = async () => {
+        'use server';
+        return 42;
+      }
+    `,
+    tsx`
+      export function Component() {
+        async function serverFunction() {
+          'use server';
+          return 42;
+        }
+
+        return <div />;
+      }
+    `,
+    tsx`
+      export function Component() {
+        const serverFunction = async () => {
+          'use server';
+          return 42;
+        }
+
+        return <div />;
+      }
+    `,
+  ],
+});
