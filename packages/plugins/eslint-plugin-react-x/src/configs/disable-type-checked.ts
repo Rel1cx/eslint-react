e80fd8c37aca3065a7dc98a03d4b9d8f7dce4cd5
Rel1cx <solarflarex@qq.com>
2026-02-06 23:45:51 +0800

    Disable implicit key rule in disable-type-checked config, closes #1477

--- packages/plugins/eslint-plugin-react-x/src/configs/disable-type-checked.ts
+++ packages/plugins/eslint-plugin-react-x/src/configs/disable-type-checked.ts
@@ -3,6 +3,7 @@ import type { RuleConfig } from "@eslint-react/shared";
 export const name = "react-x/disable-type-checked";
 
 export const rules: Record<string, RuleConfig> = {
+  "react-x/no-implicit-key": "off",
   "react-x/no-leaked-conditional-rendering": "off",
   "react-x/no-unused-props": "off",
   "react-x/prefer-read-only-props": "off",
