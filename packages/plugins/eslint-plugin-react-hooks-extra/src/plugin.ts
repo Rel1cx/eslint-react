413ae77dc8da0a379f148bf8f5d19fd7c453b5bb
Trevor Burnham <trevorburnham@gmail.com>
2026-02-16 00:09:02 -0500

    feat(react-hooks-extra): add rules-of-hooks and exhaustive-deps rules, closes #833 (#1499)

--- packages/plugins/eslint-plugin-react-hooks-extra/src/plugin.ts
+++ packages/plugins/eslint-plugin-react-hooks-extra/src/plugin.ts
@@ -2,7 +2,9 @@ import type { CompatiblePlugin } from "@eslint-react/shared";
 
 import { name, version } from "../package.json";
 
+import exhaustiveDeps from "./rules/exhaustive-deps";
 import noDirectSetStateInUseEffect from "./rules/no-direct-set-state-in-use-effect";
+import rulesOfHooks from "./rules/rules-of-hooks";
 
 export const plugin: CompatiblePlugin = {
   meta: {
@@ -10,6 +12,8 @@ export const plugin: CompatiblePlugin = {
     version,
   },
   rules: {
+    "exhaustive-deps": exhaustiveDeps,
     "no-direct-set-state-in-use-effect": noDirectSetStateInUseEffect,
+    "rules-of-hooks": rulesOfHooks,
   },
 };
