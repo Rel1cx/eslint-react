e1513578a548203e304a5ca4a25f8efb2f64ecc8
Rel1cx <solarflarex@qq.com>
2026-03-27 13:31:24 +0800

    refactor: improve checkedRequiresOnchangeOrReadonly implementation

--- examples/react-dom-with-custom-rules/.config/rules/checked-requires-onchange-or-readonly.ts
+++ examples/react-dom-with-custom-rules/.config/rules/checked-requires-onchange-or-readonly.ts
@@ -8,12 +8,13 @@ export function checkedRequiresOnchangeOrReadonly(): RuleDefinition {
       JSXOpeningElement(node) {
         const name = node.name.type === "JSXIdentifier" ? node.name.name : null;
         if (name !== "input") return;
-        const attrs = node.attributes.filter((a) => a.type === "JSXAttribute");
-        const hasChecked = attrs.some((a) => a.name.name === "checked");
-        if (!hasChecked) return;
-        const hasOnChange = attrs.some((a) => a.name.name === "onChange");
-        const hasReadOnly = attrs.some((a) => a.name.name === "readOnly");
-        if (!hasOnChange && !hasReadOnly) {
+        const attrs = new Set<string>();
+        for (const attr of node.attributes) {
+          if (attr.type === "JSXAttribute" && attr.name.type === "JSXIdentifier") {
+            attrs.add(attr.name.name);
+          }
+        }
+        if (!attrs.has("onChange") && !attrs.has("readOnly")) {
           context.report({
             node,
             message: "`checked` requires `onChange` or `readOnly`.",
