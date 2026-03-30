cacc603323077fb6d691ef655e0d80d2516ed2a4
REL1CX <solarflarex@qq.com>
2026-03-30 16:28:16 +0800

    test: enhance test coverage for eslint-plugin-react-x and eslint-plugin-react-dom rules (#1663)

--- packages/plugins/eslint-plugin-react-x/src/rules/no-direct-mutation-state/no-direct-mutation-state.spec.ts
+++ packages/plugins/eslint-plugin-react-x/src/rules/no-direct-mutation-state/no-direct-mutation-state.spec.ts
@@ -5,6 +5,7 @@ import rule, { RULE_NAME } from "./no-direct-mutation-state";
 
 ruleTester.run(RULE_NAME, rule, {
   invalid: [
+    // Basic case - direct mutation in callback within constructor
     {
       code: tsx`
         class Hello extends React.Component {
@@ -22,8 +23,95 @@ ruleTester.run(RULE_NAME, rule, {
         messageId: "default",
       }],
     },
+    // Direct mutation in lifecycle method
+    {
+      code: tsx`
+        class Hello extends React.Component {
+          componentDidMount() {
+            this.state = { foo: "bar" };
+          }
+        }
+      `,
+      errors: [{
+        messageId: "default",
+      }],
+    },
+    // Direct mutation in custom method
+    {
+      code: tsx`
+        class Hello extends React.Component {
+          handleClick() {
+            this.state = { clicked: true };
+          }
+        }
+      `,
+      errors: [{
+        messageId: "default",
+      }],
+    },
+    // Direct mutation with complex assignment
+    {
+      code: tsx`
+        class Hello extends React.Component {
+          updateState() {
+            this.state = { ...this.state, count: this.state.count + 1 };
+          }
+        }
+      `,
+      errors: [{
+        messageId: "default",
+      }],
+    },
+    // Direct mutation in arrow function property
+    {
+      code: tsx`
+        class Hello extends React.Component {
+          updateState = () => {
+            this.state = { value: 123 };
+          }
+        }
+      `,
+      errors: [{
+        messageId: "default",
+      }],
+    },
+    // Direct mutation in conditional
+    {
+      code: tsx`
+        class Hello extends React.Component {
+          componentDidUpdate() {
+            if (this.props.shouldReset) {
+              this.state = { reset: true };
+            }
+          }
+        }
+      `,
+      errors: [{
+        messageId: "default",
+      }],
+    },
+    // Direct mutation in event handler
+    {
+      code: tsx`
+        class Hello extends React.Component {
+          render() {
+            return (
+              <button onClick={() => {
+                this.state = { clicked: true };
+              }}>
+                Click
+              </button>
+            );
+          }
+        }
+      `,
+      errors: [{
+        messageId: "default",
+      }],
+    },
   ],
   valid: [
+    // Nested class component (mutation in inner class)
     tsx`
       class Foo extends React.Component {
         componentDidMount() {
@@ -35,6 +123,7 @@ ruleTester.run(RULE_NAME, rule, {
         }
       }
     `,
+    // Constructor assignment (allowed)
     tsx`
       class Hello extends React.Component {
         constructor(props) {
@@ -46,6 +135,7 @@ ruleTester.run(RULE_NAME, rule, {
         }
       }
     `,
+    // Class property initialization (allowed)
     tsx`
       import React from "react";
 
@@ -60,6 +150,86 @@ ruleTester.run(RULE_NAME, rule, {
 
         render() {
           return <div>{this.state.foo}</div>;
+        }
+      }
+    `,
+    // Using setState instead of direct mutation
+    tsx`
+      class Hello extends React.Component {
+        componentDidMount() {
+          this.setState({ foo: "bar" });
+        }
+      }
+    `,
+    // Constructor with complex state initialization
+    tsx`
+      class Hello extends React.Component {
+        constructor(props) {
+          super(props);
+          this.state = {
+            count: 0,
+            items: [],
+            loading: false,
+          };
+        }
+      }
+    `,
+    // Non-React class (should not be flagged)
+    tsx`
+      class RegularClass {
+        constructor() {
+          this.state = { value: 123 };
+        }
+
+        update() {
+          this.state = { value: 456 };
+        }
+      }
+    `,
+    // State mutation in non-component class extending plain object
+    tsx`
+      class MyClass extends SomeBaseClass {
+        update() {
+          this.state = { updated: true };
+        }
+      }
+    `,
+    // Function component using useState
+    tsx`
+      function Hello() {
+        const [state, setState] = useState({ foo: "bar" });
+        return <div>{state.foo}</div>;
+      }
+    `,
+    // Reading state (not mutating)
+    tsx`
+      class Hello extends React.Component {
+        render() {
+          return <div>{this.state.foo}</div>;
+        }
+      }
+    `,
+    // Using functional setState
+    tsx`
+      class Hello extends React.Component {
+        increment() {
+          this.setState(prevState => ({
+            count: prevState.count + 1
+          }));
+        }
+      }
+    `,
+    // Constructor with method call before assignment
+    tsx`
+      class Hello extends React.Component {
+        constructor(props) {
+          super(props);
+          this.init();
+          this.state = { initialized: true };
+        }
+
+        init() {
+          // some initialization
         }
       }
     `,
