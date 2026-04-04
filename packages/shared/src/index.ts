15d26429f6398d93c62b5228f2e53075826f9f71
Rel1cx <solarflarex@qq.com>
2026-04-04 21:29:49 +0800

    docs: remove redundant Overview headings

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,8 +1,8 @@
-export * from "./_id";
 export * from "./constants";
 export * from "./define-rule-listener";
+export * from "./id-generator";
 export * from "./react-version";
 export * from "./regexp";
 export * from "./report";
 export * from "./settings";
-export type * from "./types";
+export * from "./types";
