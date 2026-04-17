684e7f0d7137b97e1f861f57385e20cfe9f1e444
Rel1cx <solarflarex@qq.com>
2026-04-18 02:46:41 +0800

    chore: add publishConfig.access to packages, mark @local/eff as private, and update typescript

--- packages/shared/tsdown.config.ts
+++ packages/shared/tsdown.config.ts
@@ -3,11 +3,4 @@ import type { UserConfig } from "tsdown";
 
 export default {
   ...base,
-  deps: {
-    ...base.deps,
-    alwaysBundle: [
-      "@eslint-react/eslint",
-      ...(base.deps?.alwaysBundle ?? []),
-    ],
-  },
 } satisfies UserConfig;
