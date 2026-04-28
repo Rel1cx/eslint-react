7ae467ce5c87c9f3e520446e24d3b7631f3c7a9c
Rel1cx <solarflarex@qq.com>
2026-04-28 16:02:39 +0800

    release: 5.6.0-next.1

--- plugins/eslint-plugin-react-dom/src/rules/no-render-return-value/no-render-return-value.ts
+++ plugins/eslint-plugin-react-dom/src/rules/no-render-return-value/no-render-return-value.ts
@@ -1,6 +1,6 @@
 import { Check, Extract } from "@eslint-react/ast";
 import { type RuleContext, type RuleFeature, merge } from "@eslint-react/eslint";
-import { AST_NODE_TYPES as AST } from "@typescript-eslint/types";
+import { AST_NODE_TYPES as AST, TSESTree } from "@typescript-eslint/types";
 
 import { createRule } from "../../utils";
 
