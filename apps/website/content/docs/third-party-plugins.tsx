5275599ad7d6471bf76031c49d871d8866e15c6f
Rel1cx <solarflarex@qq.com>
2026-05-16 00:09:51 +0800

    Add apps/website/content/docs/third-party-plugins.mdx

---
+++ apps/website/content/docs/third-party-plugins.tsx
@@ -0,0 +1,14 @@
+export const plugins = {
+  a11y: [
+    { owner: "es-tooling", repo: "eslint-plugin-jsx-a11y-x" },
+  ],
+  fastRefresh: [
+    { owner: "ArnaudBarre", repo: "eslint-plugin-react-refresh" },
+  ],
+  importStyle: [
+    { owner: "marcalexiei", repo: "eslint-plugin-react-import" },
+  ],
+  jsxStylistic: [
+    { owner: "eslint-stylistic", repo: "eslint-stylistic" },
+  ],
+};
