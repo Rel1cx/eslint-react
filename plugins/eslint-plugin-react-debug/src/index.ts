1185491421c7264f7cc761983efad59aea0b02d7
Rel1cx <let@ik.me>
2026-09-05 20:14:55 +0800

    fix: register the same plugin object in preset configs as the default export
    Previously every preset registered the internal plugin object while the
    default export was a new object created via object spread, so registering
    the plugin manually and extending any preset resulted in ESLint reporting
    "Cannot redefine plugin". Resolve the plugin reference in each config
    lazily via a getter so it is the same object as the default export.
    
    Fixes https://github.com/Rel1cx/eslint-react/issues/1946

--- plugins/eslint-plugin-react-debug/src/index.ts
+++ plugins/eslint-plugin-react-debug/src/index.ts
@@ -4,10 +4,26 @@ import { plugin } from "./plugin";
 
 type ConfigName = "all";
 
+function createConfig(base: { plugins?: Record<string, unknown> } & Record<string, unknown>): Linter.Config {
+  return {
+    ...base,
+    plugins: {
+      ...base.plugins,
+      // Use a getter to resolve the plugin reference lazily so every config registers the same
+      // object as the default export below. Otherwise ESLint reports a "Cannot redefine plugin"
+      // error when users register the plugin manually and also extend one of the presets.
+      // See https://github.com/Rel1cx/eslint-react/issues/1946
+      get ["react-debug"]() {
+        return finalPlugin;
+      },
+    },
+  };
+}
+
 const finalPlugin: ESLint.Plugin & { configs: Record<ConfigName, Linter.Config> } = {
   ...plugin,
   configs: {
-    ["all"]: allConfig,
+    ["all"]: createConfig(allConfig),
   },
 };
 
--- plugins/eslint-plugin-react-dom/src/index.ts
+++ plugins/eslint-plugin-react-dom/src/index.ts
@@ -5,11 +5,27 @@ import { plugin } from "./plugin";
 
 type ConfigName = "recommended" | "strict";
 
+function createConfig(base: { plugins?: Record<string, unknown> } & Record<string, unknown>): Linter.Config {
+  return {
+    ...base,
+    plugins: {
+      ...base.plugins,
+      // Use a getter to resolve the plugin reference lazily so every config registers the same
+      // object as the default export below. Otherwise ESLint reports a "Cannot redefine plugin"
+      // error when users register the plugin manually and also extend one of the presets.
+      // See https://github.com/Rel1cx/eslint-react/issues/1946
+      get ["react-dom"]() {
+        return finalPlugin;
+      },
+    },
+  };
+}
+
 const finalPlugin: ESLint.Plugin & { configs: Record<ConfigName, Linter.Config> } = {
   ...plugin,
   configs: {
-    ["recommended"]: recommendedConfig,
-    ["strict"]: strictConfig,
+    ["recommended"]: createConfig(recommendedConfig),
+    ["strict"]: createConfig(strictConfig),
   },
 };
 
